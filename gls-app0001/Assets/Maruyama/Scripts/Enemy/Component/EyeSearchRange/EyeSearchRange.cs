6a64b7d0cc4f82af0fab8aa8bf0f0b126d606e66
Maru <zeal20300058@fsgcl.jp>
2021-09-16 17:46:42 +0900

    2021WIZ-8
    EySearchRangeの実装
    視界の処理に利用する。

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/EyeSearchRange/EyeSearchRange.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/EyeSearchRange/EyeSearchRange.cs
@@ -2,42 +2,141 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using System;
+
+
+/// <summary>
+/// ターゲットの管理用のパラメータ
+/// </summary>
+[Serializable]
+class EyeTargetParam
+{
+    public GameObject target;
+    public bool isFind;  //見つけた状態かどうかを返す
+
+    public EyeTargetParam(GameObject target)
+    {
+        this.target = target;
+        this.isFind = false;
+    }
+}
+
+/// <summary>
+/// 視界のパラメータの構造体
+/// </summary>
+[Serializable]
+public class EyeSearchRangeParam
+{
+    public float lenght; //索敵範囲(同心円状)
+    public float height; //索敵範囲(高さ)
+    public float rad;    //索敵範囲(角度)
+
+    public EyeSearchRangeParam()
+        :this(20.0f ,3.0f ,30.0f)
+	{}
+
+	public EyeSearchRangeParam(float lenght, float height, float deg)
+    {
+        this.lenght = lenght;
+        this.height = height;
+        this.rad = deg * Mathf.Deg2Rad;
+    }
+}
+
+
 /// <summary>
 /// 視界の管理
 /// </summary>
 public class EyeSearchRange : MonoBehaviour
 {
+    //範囲に入っているかの対象になるオブジェクト
     [SerializeField]
-    GameObject m_target;
+    List<EyeTargetParam> m_targetParams = new List<EyeTargetParam>();
 
     [SerializeField]
-    float m_range = 5.0f;
+    EyeSearchRangeParam m_param = new EyeSearchRangeParam();
 
-    void Start()
+    private void Update()
     {
-        if(m_target == null)
+        foreach(var param in m_targetParams)
         {
-            m_target = GameObject.Find("Player");
+            if (IsInEyeRange(param.target))
+            {  //ターゲットが視界に入っていたら。
+                Hit(param);
+            }
+            else
+            {
+                param.isFind = false;
+            }
         }
     }
 
-    void Update()
+    bool IsRange(GameObject target) {
+        var toVec = target.transform.position - transform.position;
+		//長さチェック
+		return toVec.magnitude <= m_param.lenght? true : false;
+	}
+
+    bool IsHeight(GameObject target) {
+		var selfPos = transform.position;
+        var targetPos = target.transform.position;
+
+        var subHeight = targetPos.y - selfPos.y;  //高さの差を求める。
+		//高さが小さかったら。
+		return Mathf.Abs(subHeight) <= m_param.height? true : false;
+	}
+
+    bool IsRad(GameObject target) {
+		var forward = transform.transform.forward;
+        forward.y = 0.0f;
+        var toVec = target.transform.position - transform.position;
+        toVec.y = 0.0f;
+
+		var newDot = Vector3.Dot(forward.normalized, toVec.normalized);
+        var newRad = Mathf.Acos(newDot);
+		//索敵範囲に入っていたら。
+		return newRad <= m_param.rad? true : false;
+	}
+
+    bool IsRay(GameObject target){
+        var toVec = target.transform.position - transform.position;
+        return !Physics.Raycast(transform.position, toVec, toVec.magnitude) ? true : false;
+	}
+
+    void Hit(EyeTargetParam targetParam) {
+		targetParam.isFind = true;
+        Debug.Log("Hit");
+	}
+
+
+    /// <summary>
+/// 視界内にいるならtrueを返す
+/// </summary>
+/// <param name="target">ターゲット</param>
+/// <returns>視界の中にいるならtrue</returns>
+    public bool IsInEyeRange(GameObject target)
     {
-        if (IsFind())
-        {
-            ChangeState();  //ステートを追従状態にする。
+        //全ての条件がtrueなら視界内に対象がいる。
+        if (IsRange(target) && IsHeight(target) && IsRad(target) && IsRay(target)){
+            return true;
+        }
+        else{
+            return false;
         }
     }
 
-    void ChangeState()
-    {
+    //アクセッサ-------------------------------------------------------------------------
 
-    }
+    public void AddTarget(GameObject target) {
+		m_targetParams.Add(new EyeTargetParam(target));
+	}
 
-    bool IsFind()
-    {
-        var toVec = m_target.transform.localPosition - transform.localPosition;
+    public void SetParam(EyeSearchRangeParam param) {
+		m_param = param;
+	}
 
-        return toVec.magnitude <= m_range ? true : false;
-    }
+     public EyeSearchRangeParam GetParam() {
+		return m_param;
+	 }
+
 }
