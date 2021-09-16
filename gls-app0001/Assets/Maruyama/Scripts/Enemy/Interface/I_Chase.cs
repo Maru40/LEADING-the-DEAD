5c75d382347ce8bb6c47e138f58be9355e736a4e
Maru <zeal20300058@fsgcl.jp>
2021-09-17 02:34:38 +0900

    2021WIZ-8
    I＿Chaseクラスの生成
    Chase用のInterface

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Interface/I_Chase.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface I_Chase
+{
+    /// <summary>
+    /// ステートの変更
+    /// </summary>
+    void ChangeState();
+}
