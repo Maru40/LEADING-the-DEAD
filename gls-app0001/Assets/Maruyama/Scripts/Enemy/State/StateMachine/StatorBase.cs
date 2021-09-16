73efea00c6d82f5a448c9f41960f77ac257b91e2
Maru <zeal20300058@fsgcl.jp>
2021-09-17 02:34:16 +0900

    2021WIZ-8
    StatorBaseの生成
    処理は未実装

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/State/StateMachine/StatorBase.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// StatorBase(現在は継承だけ(機能は後々考える。))
+/// </summary>
+public abstract class StatorBase : MonoBehaviour
+{
+
+}
