be0a80950c996b7805d302643d76ce2991fae2c6
Maru <zeal20300058@fsgcl.jp>
2021-09-17 02:35:33 +0900

    2021WIZ-8
    Stator_ZonbieNormalの実装
    ゾンビのステート管理システム

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/EnemyBase/Zombie/Nomal/State/Stator_ZombieNormal.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using StateType = ZombieNormalState;
+using TransitionMember = ZombieNormalTransitionMember;
+using StateMachine = EnemyMainStateMachine<EnemyBase, ZombieNormalState, ZombieNormalTransitionMember>;
+
+
+enum ZombieNormalState
+{
+    RandomPlowling,
+    Chase,
+}
+
+public class ZombieNormalTransitionMember
+{
+    public MyTrigger rondomPlowlingTrigger = new MyTrigger();
+    public MyTrigger chaseTrigger = new MyTrigger();
+}
+
+public class Stator_ZombieNormal : StatorBase
+{
+    StateMachine m_stateMachine;
+
+    void Start()
+    {
+        m_stateMachine = new StateMachine();
+
+        CreateStateMachine();
+    }
+
+    void Update()
+    {
+        m_stateMachine.OnUpdate();
+    }
+
+    void CreateStateMachine()
+    {
+        CreateNode();
+        CreateEdge();
+    }
+
+    void CreateNode()
+    {
+        var zombie = GetComponent<ZombieNormal>();
+
+        m_stateMachine.AddNode(StateType.RandomPlowling, new EnState_RandomPlowling(zombie));
+        m_stateMachine.AddNode(StateType.Chase,          new EnState_ChaseTarget(zombie));
+    }
+
+    void CreateEdge()
+    {
+        //ƒ‰ƒ“ƒ_ƒ€œpœj
+        m_stateMachine.AddEdge(StateType.RandomPlowling, StateType.Chase, ToChaseTrigger);
+
+        //’Ç]ˆ—
+        m_stateMachine.AddEdge(StateType.Chase, StateType.RandomPlowling, ToRandomPlowling);
+    }
+
+
+    //‘JˆÚğŒŒn---------------------------------------------------------------
+
+    bool ToChaseTrigger(TransitionMember member) {
+        return member.chaseTrigger.Get();
+    }
+
+    bool ToRandomPlowling(TransitionMember member) {
+        return member.rondomPlowlingTrigger.Get();
+    }
+
+
+    //ƒAƒNƒZƒbƒT----------------------------------------------------------------
+    
+    /// <summary>
+    /// ‘JˆÚ‚É—˜—p‚·‚éƒƒ“ƒo[‚Ìæ“¾
+    /// </summary>
+    /// <returns>‘JˆÚğŒƒƒ“ƒo[</returns>
+    public TransitionMember GetTransitionMember()
+    {
+        return m_stateMachine.GetTransitionStructMember();
+    }
+
+}
