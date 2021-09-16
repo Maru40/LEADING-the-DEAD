5333f0ac079b8c43a2554d606d21979209ab8ec6
Maru <zeal20300058@fsgcl.jp>
2021-09-16 15:38:06 +0900

    2021WIZ-6
    EnState＿RomdomPlowlingStateの生成
    ランダムに徘徊するときのステートノード

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/State/StateMachine/StateNode/BaseEnemy/EnState_RondomPlowling.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnState_RondomPlowling : EnemyStateNodeBase<EnemyBase>
+{
+    public EnState_RondomPlowling(EnemyBase owner)
+        :base(owner)
+    { }
+
+    public override void OnStart()
+    {
+        
+    }
+
+    public override void OnUpdate()
+    {
+        
+    }
+
+    public override void OnExit()
+    {
+        
+    }
+}
