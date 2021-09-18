71c198b05f0c8bba34ba55c42b18a82ad4c9f2fb
Maru <zeal20300058@fsgcl.jp>
2021-09-18 12:31:00 +0900

    2021WIZ-12
    AttackBaseの実装

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/Attack/AttackBase.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+[Serializable]
+public struct AttackParametorBase
+{
+    public float power;
+    public float startRange;  //攻撃開始範囲
+    public float damageRange; //攻撃ダメージ範囲
+
+    public AttackParametorBase(float power, float startRange, float damageRange)
+    {
+        this.power = power;
+        this.startRange = startRange;
+        this.damageRange = damageRange;
+    }
+}
+
+public abstract class AttackBase : MonoBehaviour
+{
+    [SerializeField]
+    private AttackParametorBase m_baseParam = new AttackParametorBase(1.0f, 2.0f, 1.0f);
+
+    public AttackParametorBase GetBaseParam()
+    {
+        return m_baseParam; 
+    }
+
+
+    /// <summary>
+    /// 攻撃モーション開始の距離
+    /// </summary>
+    /// <returns>距離</returns>
+    public float GetAttackStartRange()
+    {
+        return m_baseParam.startRange;
+    }
+
+    /// <summary>
+    /// 攻撃処理(アニメーションに合わせる)
+    /// </summary>
+    public abstract void Attack();
+}
