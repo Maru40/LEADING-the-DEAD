f20b8bd757317e451e6aab68a0cba4199cae609e
Itabashi Oota <itaoo0430@gmail.com>
2021-09-19 10:45:57 +0900

    2021WIZ-17
    シンバルモンキー(オブジェクト)にダメージを受ける機能(ゾンビに攻撃される機能)を追加するコンポネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/StageObjects/CymbalMonkeyDamager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CymbalMonkeyDamager : MonoBehaviour, I_TakeDamage
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(DamageData damageData)
+    {
+        Debug.Log("âéÇ…êGÇÍÇ‹ÇµÇΩ");
+    }
+}
