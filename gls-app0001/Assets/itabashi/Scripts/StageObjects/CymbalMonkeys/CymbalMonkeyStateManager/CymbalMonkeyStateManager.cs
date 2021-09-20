c500bdaf1cf1f9f12426e662b5e5019dcf5de0aa
Itabashi Oota <itaoo0430@gmail.com>
2021-09-20 16:52:59 +0900

    2021WIZ-22
    シンバルモンキー(オブジェクト)の状態を管理するコンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/StageObjects/CymbalMonkeys/CymbalMonkeyStateManager/CymbalMonkeyStateManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CymbalMonkeyStateManager : MonoBehaviour
+{
+    public enum CymbalMonkeyState
+    {
+        Normal,
+        ExplosionPreparation
+    }
+
+    public CymbalMonkeyState nowState { private set; get; } = CymbalMonkeyState.Normal;
+
+    private Rigidbody m_rigidbody;
+    private TimingDevice m_timingDevice;
+    private AlarmObject m_alarmObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+        m_timingDevice = GetComponent<TimingDevice>();
+        m_alarmObject = GetComponent<AlarmObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(nowState == CymbalMonkeyState.ExplosionPreparation)
+        {
+            return;
+        }
+
+        var groundObject = collision.gameObject.GetComponent<GroundObject>();
+
+        if(!groundObject)
+        {
+            return;
+        }
+
+        m_rigidbody.isKinematic = true;
+        m_timingDevice.TimerStart();
+        m_alarmObject.AlarmStart();
+
+        nowState = CymbalMonkeyState.ExplosionPreparation;
+    }
+}
