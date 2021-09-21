daa4645953fa630508fb82efe969cb2103a2f47b
Itabashi Oota <itaoo0430@gmail.com>
2021-09-21 13:52:43 +0900

    2021WIZ-19
    PlayerAnimationParametersにスタミナとダッシュしているかのフラグを追加した

--- gls-app0001/Assets/itabashi/Scripts/Players/PlayerAnimationParameters.cs
+++ gls-app0001/Assets/itabashi/Scripts/Players/PlayerAnimationParameters.cs
@@ -16,11 +16,22 @@ namespace Player
             set { m_playerAnimator.SetFloat("moveInput", value); }
             get { return m_playerAnimator.GetFloat("moveInput"); }
         }
+        public float stamina
+        {
+            set { m_playerAnimator.SetFloat("stamina", value); }
+            get { return m_playerAnimator.GetFloat("stamina"); }
+        }
 
         public bool isThrowingStance
         {
             set { m_playerAnimator.SetBool("isThrowingStance", value); }
             get { return m_playerAnimator.GetBool("isThrowingStance"); }
+        }
+
+        public bool isDash
+        {
+            set { m_playerAnimator.SetBool("isDash", value); }
+            get { return m_playerAnimator.GetBool("isDash"); }
         }
 
         /// <summary>
