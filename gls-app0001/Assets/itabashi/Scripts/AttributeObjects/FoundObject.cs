b84fa504f64d05fd1a7cfec442523e96ff5e30e9
Maru <zeal20300058@fsgcl.jp>
2021-09-22 14:20:20 +0900

    2021WIZ-26
    FoundObjectでデータの更新
    FoundTypeの追加

--- gls-app0001/Assets/itabashi/Scripts/AttributeObjects/FoundObject.cs
+++ gls-app0001/Assets/itabashi/Scripts/AttributeObjects/FoundObject.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class FoundObject : MonoBehaviour
 {
+    public enum FoundType
+    {
+        Player,
+        SoundObject,
+    }
+
     public struct FoundData
     {
         public int priority;
+        public FoundType type;
 
-        public FoundData(int priority)
+        public FoundData(int priority, FoundType type)
         {
             this.priority = priority;
+            this.type = type;
         }
     }
 
     [SerializeField]
-    private int m_priority;
+    private int m_priority = 0;
+
+    [SerializeField]
+    private FoundType m_type = FoundType.SoundObject;
 
     public FoundData GetFoundData()
     {
-        return new FoundData(m_priority);
+        return new FoundData(m_priority, m_type);
     }
 }
