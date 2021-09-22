5e64da8202c0526d54802dcf6ef1ae56d9f9fd53
Maru <zeal20300058@fsgcl.jp>
2021-09-22 10:31:48 +0900

    2021WIZ-23
    LinerSeekTargetで旋回速度を設定できるようにした。

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/MoveComp/ChaseTarget/UlilityEnemy/LinerSeekTarget.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/MoveComp/ChaseTarget/UlilityEnemy/LinerSeekTarget.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 public class LinerSeekTarget : NodeBase<EnemyBase>
 {
     float m_maxSpeed = 3.0f;
+    float m_turningPower = 1.0f; //ê˘âÒÇ∑ÇÈóÕ
 
     ChaseTarget m_chaseTarget;
     TargetMgr m_targetMgr;
@@ -15,13 +16,14 @@ public class LinerSeekTarget : NodeBase<EnemyBase>
     ThrongMgr m_throngMgr;
 
     public LinerSeekTarget(EnemyBase owner)
-        : this(owner,3.0f)
+        : this(owner,3.0f, 1.0f)
     { }
 
-    public LinerSeekTarget(EnemyBase owner, float maxSpeed)
+    public LinerSeekTarget(EnemyBase owner, float maxSpeed, float turningPower)
         : base(owner)
     {
         m_maxSpeed = maxSpeed;
+        m_turningPower = turningPower;
 
         m_chaseTarget = owner.GetComponent<ChaseTarget>();
         m_targetMgr = owner.GetComponent<TargetMgr>();
@@ -52,7 +54,7 @@ public class LinerSeekTarget : NodeBase<EnemyBase>
         GameObject target = m_targetMgr.GetNowTarget();
         if (target) {
             Vector3 toVec = target.transform.position - GetOwner().transform.position;
-            m_throngMgr.AvoidNearThrong(m_velocityMgr, toVec, m_maxSpeed);
+            m_throngMgr.AvoidNearThrong(m_velocityMgr, toVec, m_maxSpeed, m_turningPower);
         }
         else {
             m_chaseTarget.TargetLost();
