c6f979d9b7fa8a24471ee16c0f51c4e5bd26479f
Maru <zeal20300058@fsgcl.jp>
2021-09-23 20:45:33 +0900

    2021WIZ-30
    EnemyGeneratorでゾンビの攻撃とステータスを設定できるようにした。

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/Generator/EnemyGenerator_ZombieNormal.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Generator/EnemyGenerator_ZombieNormal.cs
@@ -10,6 +10,12 @@ public class EnemyGenerator_ZombieNormal : EnemyGenerator
     [SerializeField]
     RandomPlowlingMove.Parametor m_randomPlowlingParametor = new RandomPlowlingMove.Parametor(15.0f, 2.5f, 2.0f, 0.3f, 3.0f, 1.0f);
 
+    [SerializeField]
+    StatusManager_ZombieNormal.Status m_status = new StatusManager_ZombieNormal.Status(1.0f, 3.0f);
+
+    [SerializeField]
+    AttackParametorBase m_attackParametor = new AttackParametorBase(10.0f, 1.0f);
+
     protected override void Start()
     {
         base.Start();
@@ -27,6 +33,18 @@ public class EnemyGenerator_ZombieNormal : EnemyGenerator
         if (randomPlowling)
         {
             randomPlowling.SetParametor(m_randomPlowlingParametor);
+        }
+
+        var statusMgr = obj.GetComponent<StatusManager_ZombieNormal>();
+        if (statusMgr)
+        {
+            statusMgr.SetStatus(m_status);
+        }
+
+        var attack = obj.GetComponent<Attack_ZombieNormal>();
+        if (attack)
+        {
+            attack.SetBaseParam(m_attackParametor);
         }
     }
 }
