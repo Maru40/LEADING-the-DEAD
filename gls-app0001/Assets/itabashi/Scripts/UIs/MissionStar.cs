62859f3241c97616b815c669a6d0fb11ad26cee3
Itabashi Oota <itaoo0430@gmail.com>
2021-10-01 12:14:27 +0900

    2021WIZ-53
    達成したかしてないかで表示を変える画像コンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/UIs/MissionStar.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+
+public class MissionStar : MonoBehaviour
+{
+    /// <summary>
+    /// 達成時に表示される画像
+    /// </summary>
+    [SerializeField]
+    private Sprite m_achievementSprite;
+
+    /// <summary>
+    /// 未達成時に表示される画像
+    /// </summary>
+    [SerializeField]
+    private Sprite m_unachievedSprite;
+
+    [SerializeField]
+    private bool m_isAchieve = false;
+
+    private Image m_image;
+
+    private void Reset()
+    {
+        m_image = GetComponent<Image>();
+
+        SetAchieveSprite(m_isAchieve);
+    }
+
+    private void OnValidate()
+    {
+        m_image = GetComponent<Image>();
+        SetAchieveSprite(m_isAchieve);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SetAchieveSprite(bool isAchieve)
+    {
+        m_image.sprite = isAchieve ? m_achievementSprite : m_unachievedSprite;
+    }
+
+    public void SetIsAchieve(bool isAchieve)
+    {
+        m_isAchieve = isAchieve;
+
+        SetAchieveSprite(isAchieve);
+    }
+}
