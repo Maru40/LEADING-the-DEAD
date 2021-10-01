462a81e87c7720bb120629004a6ba3a431ba10f8
Maru <zeal20300058@fsgcl.jp>
2021-10-02 05:00:55 +0900

    2021WIZ-51
    RespawnStatusUpBaseの実装

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Respawn/StatusUp/EnemyRespawnStatusUpBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+/// <summary>
+/// エネミーリスポーン時にステータスの工場
+/// </summary>
+public abstract class EnemyRespawnStatusUpBase : MonoBehaviour
+{
+    private float m_respawnCount = 0;
+
+    abstract protected void StatusUp();
+
+    virtual public void Respawn()
+    {
+        m_respawnCount++;
+    }
+}
