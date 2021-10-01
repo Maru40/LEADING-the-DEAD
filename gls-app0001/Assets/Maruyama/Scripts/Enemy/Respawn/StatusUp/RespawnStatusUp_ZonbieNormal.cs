6ca0fef149c3e0502fd4766d3fa0ac56ee1ea7d6
Maru <zeal20300058@fsgcl.jp>
2021-10-02 05:00:21 +0900

    2021WIZ-51
    EnemyStatusUpBaseの生成
    Respawnするときに、ステータスが変更できるようにした。

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Respawn/StatusUp/RespawnStatusUp_ZonbieNormal.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnStatusUp_ZonbieNormal : EnemyRespawnStatusUpBase
+{
+    [SerializeField]
+    CreateSetParametor_ZombieNormal m_param = new CreateSetParametor_ZombieNormal(0.0f);
+
+    protected override void StatusUp()
+    {
+        var respawn = GetComponent<EnemyRespawnManager>();
+        if (respawn)
+        {
+            respawn.AddParametor(m_param.respawnParametor);
+        }
+
+        var chase = GetComponent<ChaseTarget>();
+        if (chase)
+        {
+            chase.AddParametor(m_param.chaseParametor);
+        }
+
+        var randomPlowling = GetComponent<RandomPlowlingMove>();
+        if (randomPlowling)
+        {
+            randomPlowling.AddParametor(m_param.randomPlowlingParametor);
+        }
+
+        var statusMgr = GetComponent<StatusManager_ZombieNormal>();
+        if (statusMgr)
+        {
+            statusMgr.AddStatus(m_param.status);
+        }
+
+        var attack = GetComponent<Attack_ZombieNormal>();
+        if (attack)
+        {
+            attack.AddBaseParam(m_param.attackParametor);
+        }
+    }
+
+    public override void Respawn()
+    {
+        base.Respawn();
+
+        StatusUp();
+    }
+}
