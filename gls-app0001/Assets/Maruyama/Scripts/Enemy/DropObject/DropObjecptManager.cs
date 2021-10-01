58715d927b11861da26ecf18fba3f58766834294
Maru <zeal20300058@fsgcl.jp>
2021-10-02 06:31:11 +0900

    2021WIZ-60
    DropObjectファイルを生成して、DropObjectManagerを生成した。

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/DropObject/DropObjecptManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+[Serializable]
+struct DropData
+{
+    public GameObject obj;
+    public float probability;  //ドロップする確率
+}
+
+public class DropObjecptManager : MonoBehaviour
+{
+    [SerializeField]
+    List<DropData> m_datas = new List<DropData>();
+
+    public void Drop()
+    {
+        foreach(var data in m_datas)
+        {
+            
+        }   
+    }
+}
