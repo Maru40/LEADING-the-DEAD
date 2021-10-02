afcecf9ca633899e8cfbaf9701d998c1d04beb31
Maru <zeal20300058@fsgcl.jp>
2021-10-02 15:15:26 +0900

    2021WIZ-60
    DropObjectManagerでドロップアイテムがある場合に生成するようにした。

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/DropObject/DropObjecptManager.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/DropObject/DropObjecptManager.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MaruUtility;
 
 using System;
 
 [Serializable]
-struct DropData
+public class DropData
 {
     public GameObject obj;
     public float probability;  //ドロップする確率
+
+    public DropData(GameObject obj, float probability)
+    {
+        this.obj = obj;
+        this.probability = probability;
+    }
 }
 
 public class DropObjecptManager : MonoBehaviour
@@ -20,7 +27,31 @@ public class DropObjecptManager : MonoBehaviour
     {
         foreach(var data in m_datas)
         {
-            
+            if(data == null) {
+                continue;
+            }
+
+            var isDrop = MyRandom.RandomProbability(data.probability);
+            if (isDrop)  //ドロップするなら。
+            {
+                //オブジェクトの生成。
+                Instantiate(data.obj, transform.position, Quaternion.identity);
+                //演出の生成(particleとか？)
+
+            }
         }   
+    }
+
+    
+    //アクセッサ------------------------------------------------------------------
+
+    public void AddData(DropData data)
+    {
+        m_datas.Add(data);
+    }
+
+    public void RemoveData(DropData data)
+    {
+        m_datas.Remove(data);
     }
 }
