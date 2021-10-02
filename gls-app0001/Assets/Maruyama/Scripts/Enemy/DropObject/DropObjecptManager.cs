bc2c3e23b41b787748b146be3a68e2978dfd1eae
Maru <zeal20300058@fsgcl.jp>
2021-10-02 16:53:38 +0900

    2021WIZ-60
    DropDataにparticleのデータを持たせ、
    所持しているゾンビにparticleを発生するようにした。

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/DropObject/DropObjecptManager.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/DropObject/DropObjecptManager.cs
@@ -10,11 +10,17 @@ public class DropData
 {
     public GameObject obj;
     public float probability;  //ドロップする確率
+    public GameObject particle;
 
     public DropData(GameObject obj, float probability)
+        :this(obj, probability, null)
+    { }
+
+    public DropData(GameObject obj, float probability, GameObject particle)
     {
         this.obj = obj;
         this.probability = probability;
+        this.particle = particle;
     }
 }
 
@@ -23,6 +29,20 @@ public class DropObjecptManager : MonoBehaviour
     [SerializeField]
     List<DropData> m_datas = new List<DropData>();
 
+    Dictionary<DropData ,GameObject> m_particles = new Dictionary<DropData, GameObject>();
+
+    void InstatiateParticle(DropData data)
+    {
+        if (data.particle != null)
+        {
+            var particle = Instantiate(data.particle, transform.position, Quaternion.identity, transform);
+            m_particles[data] = particle;
+        }
+    }
+
+    /// <summary>
+    /// アイテムのドロップ
+    /// </summary>
     public void Drop()
     {
         foreach(var data in m_datas)
@@ -48,10 +68,36 @@ public class DropObjecptManager : MonoBehaviour
     public void AddData(DropData data)
     {
         m_datas.Add(data);
+
+        InstatiateParticle(data);
     }
 
     public void RemoveData(DropData data)
     {
+        if (m_particles.ContainsKey(data))  //存在したら
+        {
+            Destroy(m_particles[data]);
+            m_particles.Remove(data);
+        }
+
         m_datas.Remove(data);
+    }
+
+    public void RemoveDatas(List<DropData> datas)
+    {
+        for(int i = 0; i < datas.Count; i++)
+        {
+            RemoveData(datas[i]);
+        }
+    }
+
+    public int GetNumData()
+    {
+        return m_datas.Count;
+    }
+
+    public List<DropData> GetDatas()
+    {
+        return m_datas;
     }
 }
