eb1570b9af3238391ed3f1fe1d2d9a1f1d75283a
Maru <zeal20300058@fsgcl.jp>
2021-10-02 15:08:40 +0900

    2021WIZ-60
    MyRandomクラスの実装

---
+++ gls-app0001/Assets/Maruyama/Scripts/Utility/MaruUtility/Random/MyRandom.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MaruUtility
+{
+
+	public class MyRandom
+	{
+		static System.Random sm_random = new System.Random(System.DateTime.Now.Millisecond);
+
+		/// <summary>
+		/// 何割の確率化変数で受け取る。
+		/// 0～100までの間の数値を引き数で受け取る。
+		/// </summary>
+		/// <param name="prob">確率</param>
+		/// <returns>確率でtrueかfalseを返す</returns>
+		public static bool RandomProbability(float probability)
+		{
+			var random = sm_random.Next(0, 100);
+			return random <= probability ? true : false;
+		}
+
+		public static int RandomValue(int min, int max)
+        {
+			return sm_random.Next(min, max);
+        }
+	}
+
+}
+
+
+
