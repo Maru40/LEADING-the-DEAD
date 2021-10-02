2ebc1780ddec8287b6464c58110705c0aa9e36d5
Itabashi Oota <itaoo0430@gmail.com>
2021-10-02 13:45:50 +0900

    2021WIZ-56
    ゲームの一時停止と再開を管理するスタティッククラスを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/GameManager/GameTimeManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UniRx;
+
+public class GameTimeManager
+{
+    private static BoolReactiveProperty m_isPause = new BoolReactiveProperty(false);
+
+    public static System.IObservable<bool> isPauseOnChanged { get { return m_isPause; } }
+
+    public static bool isPause { get { return m_isPause.Value; } }
+
+    public static void Pause()
+    {
+        Time.timeScale = 0.0f;
+
+        m_isPause.Value = true;
+    }
+
+    public static void UnPause()
+    {
+        Time.timeScale = 1.0f;
+
+        m_isPause.Value = false;
+    }
+}
