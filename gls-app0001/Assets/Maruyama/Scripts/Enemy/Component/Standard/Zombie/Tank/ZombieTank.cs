1b901c851f691196ec015a0f94c6beda956b8384
Maru <zeal20300058@fsgcl.jp>
2021-10-03 21:49:22 +0900

    2021WIZ-48
    ZombieTankの実装

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/Standard/Zombie/Tank/ZombieTank.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/Standard/Zombie/Tank/ZombieTank.cs
@@ -2,17 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZombieTank : MonoBehaviour
+public class ZombieTank : EnemyBase, I_Chase, I_Listen
 {
-    // Start is called before the first frame update
+    //コンポーネント系
+
+    Stator_ZombieTank m_stator;
+    TargetManager m_targetMgr;
+
     void Start()
+    {
+        m_stator = GetComponent<Stator_ZombieTank>();
+        m_targetMgr = GetComponent<TargetManager>();
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    //インターフェースの実装-------------------------------------------------
+
+    void I_Chase.ChangeState()
     {
-        
+        var member = m_stator.GetTransitionMember();
+        member.chaseTrigger.Fire();
+    }
+
+    void I_Listen.Listen(FoundObject foundObject)
+    {
+        //ターゲットの切替
+        m_targetMgr.SetNowTarget(GetType(), foundObject);
+
+        var member = m_stator.GetTransitionMember();
+        member.chaseTrigger.Fire();
     }
 }
