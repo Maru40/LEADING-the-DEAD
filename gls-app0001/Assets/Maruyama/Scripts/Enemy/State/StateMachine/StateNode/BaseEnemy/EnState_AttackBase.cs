70b46a7fa1499381e29efc6d368ef64986ca4b2e
Maru <zeal20300058@fsgcl.jp>
2021-10-03 21:53:58 +0900

    2021WIZ-48
    EnStaet＿AttackをAttackBaseに変更

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/State/StateMachine/StateNode/BaseEnemy/EnState_AttackBase.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnState_AttackBase : EnemyStateNodeBase<EnemyBase>
+{
+    public EnState_AttackBase(EnemyBase owner)
+        :base(owner)
+    { }
+
+    /// <summary>
+    /// 攻撃するアニメーションの再生
+    /// </summary>
+    protected abstract void PlayStartAnimation();
+
+    /// <summary>
+    /// 切り替えるコンポーネントの準備
+    /// </summary>
+    protected override void ReserveChangeComponents()
+    {
+        var owner = GetOwner();
+
+        AddChangeComp(owner.GetComponent<ObstacleEvasion>(), false, true);
+    }
+
+    public override void OnStart()
+    {
+        PlayStartAnimation();
+
+        base.OnStart();
+    }
+
+    public override void OnUpdate()
+    {
+
+    }
+
+    public override void OnExit()
+    {
+        base.OnExit();
+    }
+}
