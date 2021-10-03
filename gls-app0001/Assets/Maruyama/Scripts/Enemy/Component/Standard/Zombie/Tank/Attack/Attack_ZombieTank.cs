0831b551db22cff85b93661604b619cc6d8a7d76
Maru <zeal20300058@fsgcl.jp>
2021-10-04 04:18:48 +0900

    2021WIZ-48
    Attack＿ZombieTankでステートの変更まで記述
    攻撃詳細は別チケットで実装

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/Standard/Zombie/Tank/Attack/Attack_ZombieTank.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/Standard/Zombie/Tank/Attack/Attack_ZombieTank.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using MaruUtility;
+
 public class Attack_ZombieTank : AttackBase
 {
+    TargetManager m_targetMgr;
+    Stator_ZombieTank m_stator;
+
     void Start()
     {
-        
+        m_targetMgr = GetComponent<TargetManager>();
+        m_stator = GetComponent<Stator_ZombieTank>();
     }
 
     void Update()
@@ -16,12 +22,19 @@ public class Attack_ZombieTank : AttackBase
 
     public override bool IsAttackStartRange()
     {
-        return true;
+        float range = GetBaseParam().startRange;
+        FoundObject target = m_targetMgr.GetNowTarget();
+        if (target) {
+            return Calculation.IsRange(gameObject, target.gameObject, range);
+        }
+        else {
+            return false;
+        }
     }
 
     public override void AttackStart()
     {
-
+        m_stator.GetTransitionMember().attackTrigger.Fire();
     }
 
     public override void Attack()
@@ -29,12 +42,12 @@ public class Attack_ZombieTank : AttackBase
 
     }
 
-    public override void EndAnimationEvent()
+    public override void AttackHitEnd()
     {
 
     }
 
-    public override void AttackHitEnd()
+    public override void EndAnimationEvent()
     {
 
     }
