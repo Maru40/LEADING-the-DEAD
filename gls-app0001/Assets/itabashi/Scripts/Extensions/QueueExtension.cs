b7939ffc2712f36c1e33a2672ca11419e00864a1
Itabashi Oota <itaoo0430@gmail.com>
2021-10-04 15:38:29 +0900

    2021WIZ-80
    QueueにEmpty関数を追加

---
+++ gls-app0001/Assets/itabashi/Scripts/Extensions/QueueExtension.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class QueueExtension
+{
+    public static bool Empty<T>(this Queue<T> queue)
+    {
+        return queue.Count == 0;
+    }
+}
