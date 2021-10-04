0dea6e23e51b573d6b21691af4b0be939ae16837
Itabashi Oota <itaoo0430@gmail.com>
2021-10-04 15:40:06 +0900

    2021WIZ-80
    UseItemUIとPlayerPickUpperを関連付けるコンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/UIs/UseItemUIPresenter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class UseItemUIPresenter : MonoBehaviour
+{
+    [SerializeField]
+    private UseItemUI m_useItemUI;
+
+    [SerializeField]
+    private PlayerPickUpper m_playerPickUpper;
+
+    private void Awake()
+    {
+        m_playerPickUpper.stackObjectsCountOnChanged.Subscribe(count => CountChangedEvent(count));
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CountChangedEvent(int count)
+    {
+        m_useItemUI.isValidity = count > 0;
+        m_useItemUI.count = count;
+        
+    }
+}
