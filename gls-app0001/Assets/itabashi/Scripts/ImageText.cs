73fe2cba65741386b632d48b5dd32a3e86bcf379
Itabashi Oota <itaoo0430@gmail.com>
2021-10-04 12:15:16 +0900

    2021WIZ-59
    背景とテキストを扱えるコンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/ImageText.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ImageText : MonoBehaviour
+{
+    [SerializeField]
+    private Image m_backGround;
+
+    [SerializeField]
+    private Text m_text;
+
+    public Image backGround { get { return m_backGround; } }
+
+    public Text text { get { return m_text; } }
+
+    private void UpdateActive(bool isActive)
+    {
+        m_backGround.gameObject.SetActive(isActive);
+        m_text.gameObject.SetActive(isActive);
+    }
+
+    private void OnEnable()
+    {
+        UpdateActive(true);
+    }
+
+    private void OnDisable()
+    {
+        UpdateActive(false);
+    }
+}
