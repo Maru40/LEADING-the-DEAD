b7b179c2697082e56923d3e6490210b96dc0eadb
Maru <zeal20300058@fsgcl.jp>
2021-10-05 14:02:50 +0900

    2021WIZ-73
    SmalObjectRnageの生成

---
+++ gls-app0001/Assets/Maruyama/Scripts/Item/BloodBag/SmellObject/SmellRange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmellRange : MonoBehaviour
+{
+    //匂いの効果のある範囲
+    [SerializeField]
+    Collider m_smellRange = null;
+    
+    void Start()
+    {
+        
+    }
+
+ 
+    void Update()
+    {
+        
+    }
+
+    bool IsRange()
+    {
+        return true;
+    }
+}
