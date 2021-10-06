0fd07b41702535140ee5d81533f9e9049983fc7f
Itabashi Oota <itaoo0430@gmail.com>
2021-10-06 11:03:34 +0900

    2021WIZ-77
    UniRx拡張メソッド用クラスの作成

---
+++ gls-app0001/Assets/itabashi/Scripts/Extensions/UnirxIObserveExtension.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+public static class UniRxIObservableExtension
+{
+    public struct BeforeAfterData<T>
+    {
+        public T before;
+        public T after;
+
+        public BeforeAfterData(T before,T after)
+        {
+            this.before = before;
+            this.after = after;
+        }
+    }
+
+    public static IObservable<BeforeAfterData<T>> BeforeAfter<T>(this IObservable<T> observable)
+    {
+        return observable.Zip(observable.Skip(1), (T before, T after) => new BeforeAfterData<T>(before, after));
+    }
+}
