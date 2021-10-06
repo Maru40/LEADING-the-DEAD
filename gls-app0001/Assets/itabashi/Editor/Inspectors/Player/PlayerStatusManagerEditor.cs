64d81b44190c2e0ed41e994cfe9d1e6e40cfabee
Itabashi Oota <itaoo0430@gmail.com>
2021-10-06 11:03:53 +0900

    2021WIZ-77
    PlayerStatusManagerEditorの更新

--- gls-app0001/Assets/itabashi/Editor/Inspectors/Player/PlayerStatusManagerEditor.cs
+++ gls-app0001/Assets/itabashi/Editor/Inspectors/Player/PlayerStatusManagerEditor.cs
@@ -16,50 +16,22 @@ namespace Player
         /// スタミナ関係の折り畳み
         /// </summary>
         private bool m_stamitaFoldout = true;
+        /// <summary>
+        /// 状態異常関係の折り畳み
+        /// </summary>
+        private bool m_abnormalStateFoldout = true;
+
+        private bool m_stunFoldout = false;
+
         public override void OnInspectorGUI()
         {
             serializedObject.Update();
 
-            m_hpFoldout = EditorGUILayout.Foldout(m_hpFoldout, "HP");
+            HpGUILayout();
 
-            var playerStatusManager = (PlayerStatusManager)target;
+            StaminaGUILayout();
 
-            if(m_hpFoldout)
-            {
-                var hp = serializedObject.FindProperty("m_hp");
-                var maxHp = serializedObject.FindProperty("m_maxHp");
-                var hpGauge = serializedObject.FindProperty("m_hpGauge");
-
-                EditorGUILayout.PropertyField(hpGauge, new GUIContent("    HPゲージ"));
-                EditorGUILayout.PropertyField(maxHp, new GUIContent("    体力の最大値"));
-
-                hp.floatValue = Mathf.Min(hp.floatValue, maxHp.floatValue);
-
-                EditorGUILayout.Slider(hp, 0.0f, maxHp.floatValue,new GUIContent("    現在の体力"));
-            }
-
-            m_stamitaFoldout = EditorGUILayout.Foldout(m_stamitaFoldout, "スタミナ");
-
-            if (m_stamitaFoldout)
-            {
-                var stamina = serializedObject.FindProperty("m_stamina");
-                var maxStamina = serializedObject.FindProperty("m_maxStamina");
-                var staminaGauge = serializedObject.FindProperty("m_staminaGauge");
-                var staminaRecoveryPerSeconds = serializedObject.FindProperty("m_staminaRecoveryPerSeconds");
-                var updateStaminaEvent = serializedObject.FindProperty("m_updateStaminaEvent");
-
-                EditorGUILayout.PropertyField(staminaGauge, new GUIContent("    スタミナゲージ"));
-                EditorGUILayout.PropertyField(maxStamina, new GUIContent("    スタミナの最大値"));
-
-                stamina.floatValue = Mathf.Min(stamina.floatValue, maxStamina.floatValue);
-
-                EditorGUILayout.Slider(stamina, 0.0f, maxStamina.floatValue, new GUIContent("    現在のスタミナ"));
-
-                EditorGUILayout.PropertyField(staminaRecoveryPerSeconds, new GUIContent("    回復スタミナ量/秒"));
-
-                EditorGUILayout.Space();
-                EditorGUILayout.PropertyField(updateStaminaEvent, new GUIContent("スタミナ更新イベント"));
-            }
+            AbnormalGUILayout();
 
             EditorGUILayout.Space();
 
@@ -67,6 +39,108 @@ namespace Player
             EditorGUILayout.PropertyField(deadEvent, new GUIContent("死亡イベント"));
 
             serializedObject.ApplyModifiedProperties();
+        }
+
+        private void HpGUILayout()
+        {
+            m_hpFoldout = EditorGUILayout.Foldout(m_hpFoldout, "HP");
+
+            if (!m_hpFoldout)
+            {
+                return;
+            }
+
+            var hp = serializedObject.FindProperty("m_hp");
+            var maxHp = serializedObject.FindProperty("m_maxHp");
+            var hpGauge = serializedObject.FindProperty("m_hpGauge");
+
+            EditorGUI.indentLevel++;
+
+            EditorGUILayout.PropertyField(hpGauge, new GUIContent("HPゲージ"));
+            EditorGUILayout.PropertyField(maxHp, new GUIContent("体力の最大値"));
+
+            hp.floatValue = Mathf.Min(hp.floatValue, maxHp.floatValue);
+
+            EditorGUILayout.Slider(hp, 0.0f, maxHp.floatValue, new GUIContent("現在の体力"));
+
+            EditorGUI.indentLevel--;
+        }
+
+        private void StaminaGUILayout()
+        {
+            m_stamitaFoldout = EditorGUILayout.Foldout(m_stamitaFoldout, "スタミナ");
+
+            if (!m_stamitaFoldout)
+            {
+                return;
+            }
+
+            var stamina = serializedObject.FindProperty("m_stamina");
+            var maxStamina = serializedObject.FindProperty("m_maxStamina");
+            var staminaGauge = serializedObject.FindProperty("m_staminaGauge");
+            var staminaRecoveryPerSeconds = serializedObject.FindProperty("m_staminaRecoveryPerSeconds");
+            var updateStaminaEvent = serializedObject.FindProperty("m_updateStaminaEvent");
+
+            EditorGUI.indentLevel++;
+
+            EditorGUILayout.PropertyField(staminaGauge, new GUIContent("スタミナゲージ"));
+            EditorGUILayout.PropertyField(maxStamina, new GUIContent("スタミナの最大値"));
+
+            stamina.floatValue = Mathf.Min(stamina.floatValue, maxStamina.floatValue);
+
+            EditorGUILayout.Slider(stamina, 0.0f, maxStamina.floatValue, new GUIContent("現在のスタミナ"));
+
+            EditorGUILayout.PropertyField(staminaRecoveryPerSeconds, new GUIContent("回復スタミナ量/秒"));
+
+            EditorGUILayout.Space();
+            EditorGUILayout.PropertyField(updateStaminaEvent, new GUIContent("スタミナ更新イベント"));
+
+            EditorGUI.indentLevel--;
+        }
+
+        private void AbnormalGUILayout()
+        {
+            m_abnormalStateFoldout = EditorGUILayout.Foldout(m_abnormalStateFoldout, "状態異常");
+
+            if(!m_abnormalStateFoldout)
+            {
+                return;
+            }
+
+            EditorGUI.indentLevel++;
+
+            StunGUILayout();
+
+            EditorGUI.indentLevel--;
+        }
+
+        private void StunGUILayout()
+        {
+            EditorGUILayout.BeginHorizontal();
+            {
+                m_stunFoldout = EditorGUILayout.Foldout(m_stunFoldout, "スタン");
+            }
+            EditorGUILayout.EndHorizontal();
+
+            if(!m_stunFoldout)
+            {
+                return;
+            }
+
+            EditorGUI.indentLevel++;
+
+            var isStun = serializedObject.FindProperty("m_isStun");
+
+            EditorGUILayout.PropertyField(isStun);
+
+            var stunSecound = serializedObject.FindProperty("m_stunSecond");
+
+            EditorGUILayout.PropertyField(stunSecound, new GUIContent("スタン時間(秒)"));
+
+            stunSecound.floatValue = Mathf.Max(stunSecound.floatValue, 0.0f);
+
+            EditorGUI.indentLevel--;
+
         }
     }
 }
