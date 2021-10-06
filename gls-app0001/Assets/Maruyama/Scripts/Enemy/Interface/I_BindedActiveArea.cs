b974815d6418d9ae3a62cf894bce9cdfe04d8df5
Maru <zeal20300058@fsgcl.jp>
2021-10-06 17:06:42 +0900

    2021WIZ-65
    I_BindedActivateAreaの生成

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Interface/I_BindedActiveArea.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface I_BindedActiveArea
+{
+    public void Bind(BindActivateArea other);
+    public void BindRelease(BindActivateArea other);
+}
