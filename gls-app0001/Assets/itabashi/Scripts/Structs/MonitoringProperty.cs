20be6de8840dd161d2a44b95874ced2b19cabebd
Itabashi Oota <itaoo0430@gmail.com>
2021-10-06 11:00:55 +0900

    2021WIZ-77
    値と変更イベントをもった構造体を作成

---
+++ gls-app0001/Assets/itabashi/Scripts/Structs/MonitoringProperty.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Utility
+{
+    [System.Serializable]
+    public class MonitoringProperty<T>
+    {
+        [SerializeField]
+        private T m_value;
+
+        [SerializeField]
+        public UnityEvent<T> OnChangedEvent;
+
+        public T value
+        {
+            set
+            {
+                bool isEqual = m_value.Equals(value);
+
+                m_value = value;
+
+                if(!isEqual)
+                {
+                    OnChangedEvent?.Invoke(m_value);
+                }
+            }
+
+            get { return m_value; }
+        }
+
+
+        public MonitoringProperty(T value)
+        {
+            m_value = value;
+        }
+    }
+}
