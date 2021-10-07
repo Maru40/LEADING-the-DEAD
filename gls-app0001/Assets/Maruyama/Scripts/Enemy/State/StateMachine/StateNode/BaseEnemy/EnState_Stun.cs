bb83d016ded0b3b60886da024fe95e32ca43e890
Maru <zeal20300058@fsgcl.jp>
2021-10-07 15:56:03 +0900

    2021WIZ-66
    EnState_Stunノードの生成

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/State/StateMachine/StateNode/BaseEnemy/EnState_Stun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnState_Stun : EnemyStateNodeBase<EnemyBase>
+{
+    
+
+    public EnState_Stun(EnemyBase owner)
+        : base(owner)
+    {
+
+    }
+
+    protected override void ReserveChangeComponents()
+    {
+
+    }
+
+    public override void OnStart()
+    {
+        base.OnStart();
+    }
+
+    public override void OnUpdate()
+    {
+
+    }
+}
