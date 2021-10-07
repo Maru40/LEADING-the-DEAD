1d09f6f51d46a2a7b4d865b4e0f50523867678ba
Maru <zeal20300058@fsgcl.jp>
2021-10-07 11:37:39 +0900

    2021WIZ-65
    ZombieNormalでバインドされたときの処理を実装

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/Standard/Zombie/Nomal/ZombieNormal.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/Standard/Zombie/Nomal/ZombieNormal.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZombieNormal : EnemyBase, I_Chase, I_Listen
+public class ZombieNormal : EnemyBase, I_Chase, I_Listen, I_BindedActiveArea
 {
+    //test用に表示したり、消したりする用。
+    [SerializeField]
+    GameObject m_tempPrehab = null;
+    GameObject m_tempObject = null;
+
     //コンポーネント系
 
     Stator_ZombieNormal m_stator;
     TargetManager m_targetMgr;
 
-    void Start()
+    RandomPlowlingMove m_randomPlowling;
+    ThrongManager m_throngMgr;
+
+    void Awake()
     {
         m_stator = GetComponent<Stator_ZombieNormal>();
         m_targetMgr = GetComponent<TargetManager>();
+
+        m_randomPlowling = GetComponent<RandomPlowlingMove>();
+        m_throngMgr = GetComponent<ThrongManager>();
     }
 
     void Update()
@@ -35,5 +46,30 @@ public class ZombieNormal : EnemyBase, I_Chase, I_Listen
 
         var member = m_stator.GetTransitionMember();
         member.chaseTrigger.Fire();
+    }
+
+    void I_BindedActiveArea.Bind(BindActivateArea bind)
+    {
+        m_randomPlowling.SetCenterObject(bind.gameObject);
+        m_randomPlowling.SetRandomPositionRadius(bind.GetBindRange());
+        m_throngMgr.enabled = false;
+
+        m_stator.GetTransitionMember().rondomPlowlingTrigger.Fire();
+
+        //ターゲットがBindオブジェクトと同じならnullにする。
+        var found = bind.GetComponent<FoundObject>();
+        if (found)
+        {
+            var target = m_targetMgr.GetNowTarget();
+            if(found == target)
+            {
+                m_targetMgr.SetNowTarget(GetType(), null);
+            }
+        }
+    }
+
+    void I_BindedActiveArea.BindRelease(BindActivateArea bind)
+    {
+        m_randomPlowling.ResetCenterObject();
     }
 }
