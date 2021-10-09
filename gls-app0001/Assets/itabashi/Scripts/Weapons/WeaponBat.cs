ad0ad72b967e9bbc565ae3f66e4355c98d48f2a9
Itabashi Oota <itaoo0430@gmail.com>
2021-10-09 11:18:25 +0900

    2021WIZ-68
    バットを表すコンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/Weapons/WeaponBat.cs
@@ -0,0 +1,24 @@
+﻿using AttributeObject;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBat : WeaponBase
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected override void OnDamageTheOpponent(TakeDamageObject takeDamageObject, DamageData baseDamageData)
+    {
+        takeDamageObject.TakeDamage(baseDamageData);
+    }
+}
