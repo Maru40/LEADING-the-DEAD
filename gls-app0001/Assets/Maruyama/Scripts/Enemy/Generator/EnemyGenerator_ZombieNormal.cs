0d0cd333088f35b44c8de195fa146e7fc490a8ad
Maru <zeal20300058@fsgcl.jp>
2021-10-10 03:05:06 +0900

    2021WIZ-67
    BaseAttackで攻撃のspeedを設けるのをやめた。

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/Generator/EnemyGenerator_ZombieNormal.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Generator/EnemyGenerator_ZombieNormal.cs
@@ -14,7 +14,7 @@ public class CreateSetParametor_ZombieNormal
     public ChaseTargetParametor chaseParametor = new ChaseTargetParametor(0.75f, 3.0f, 3.0f, 10.0f, 3.0f);
     public RandomPlowlingMove.Parametor randomPlowlingParametor = new RandomPlowlingMove.Parametor(15.0f, 2.5f, 2.0f, 0.3f, 3.0f, 1.0f);
     public StatusManager_ZombieNormal.Status status = new StatusManager_ZombieNormal.Status(1.0f, 3.0f);
-    public AttackParametorBase attackParametor = new AttackParametorBase(10.0f, 2.5f, 3.0f);
+    public AttackParametorBase attackParametor = new AttackParametorBase(10.0f, 2.5f);
 
     public CreateSetParametor_ZombieNormal()
     {
@@ -27,7 +27,7 @@ public class CreateSetParametor_ZombieNormal
         chaseParametor = new ChaseTargetParametor(allInit, allInit, allInit, allInit, allInit);
         randomPlowlingParametor = new RandomPlowlingMove.Parametor(allInit, allInit, allInit, allInit, allInit, allInit);
         status = new StatusManager_ZombieNormal.Status(allInit, allInit);
-        attackParametor = new AttackParametorBase(allInit, allInit, allInit);
+        attackParametor = new AttackParametorBase(allInit, allInit);
     }
 }
 
