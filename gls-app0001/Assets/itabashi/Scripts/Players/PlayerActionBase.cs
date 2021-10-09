b40695483cfbcc725c20a6c422b706e9291f3cb0
Itabashi Oota <itaoo0430@gmail.com>
2021-10-09 11:19:48 +0900

    2021WIZ-68
    プレイヤーのアクションの基本クラスを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/Players/PlayerActionBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public abstract class PlayerActionBase : MonoBehaviour
+    {
+        [SerializeField]
+        protected PlayerStatusManager m_playerStatusManager;
+    }
+}
