1c7b23e57bf64359f1b79c9e4fc68426a81d7f28
Maru <zeal20300058@fsgcl.jp>
2021-10-10 03:04:10 +0900

    2021WIZ-67
    StatusManager＿ZombieTankでいらないコメント削除

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/Standard/Zombie/Tank/Status/StatusManager_ZombieTank.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/Standard/Zombie/Tank/Status/StatusManager_ZombieTank.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class StatusManager_ZombieTank : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
