6b5b6a39e69647bad3a6e2a122d28ad08b76de22
Maru <zeal20300058@fsgcl.jp>
2021-10-12 02:47:11 +0900

    2021WIZ-87
    StateNode_ZombieNormal_Angerの実装

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/Standard/Zombie/Nomal/Stator/StateNode/StateNode_ZombieNormal_Anger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateNode_ZombieNormal_Anger : EnState_Anger
+{
+    StatusManager_ZombieNormal m_stator;
+
+    public StateNode_ZombieNormal_Anger(EnemyBase owner)
+        : base(owner)
+    {
+        m_stator = owner.GetComponent<StatusManager_ZombieNormal>();
+    }
+
+    protected override void ReserveChangeComponents()
+    {
+        base.ReserveChangeComponents();
+    }
+
+    public override void OnStart()
+    {
+        base.OnStart();
+
+        m_stator.SetIsAnger(true);
+    }
+
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+    }
+}
