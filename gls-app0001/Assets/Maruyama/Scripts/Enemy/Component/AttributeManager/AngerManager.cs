f8ec6494525cc2c3ca8261d67c12b3ce604e4423
Maru <zeal20300058@fsgcl.jp>
2021-10-12 16:01:52 +0900

    2021WIZ-87
    AngerMangerで怒り状態になったら、BuffParametorに怒り状態用のバフがかかったことを伝えるようにした。

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/AttributeManager/AngerManager.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/AttributeManager/AngerManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using System;
+using UniRx;
 
 /// <summary>
 /// 怒り状態の管理
@@ -12,41 +13,86 @@ public class AngerManager : MonoBehaviour
     [Serializable]
     public struct RiseParametor
     {
-        float attack;
-        float speed;
+        public float attackPower;
+        public float speed;
 
-        public RiseParametor(float attack,float speed)
+        public RiseParametor(float attackPower, float speed)
         {
-            this.attack = attack;
+            this.attackPower = attackPower;
             this.speed = speed;
+        }
+
+        public static RiseParametor operator +(RiseParametor right, RiseParametor left)
+        {
+            var param = new RiseParametor();
+            param.attackPower = right.attackPower + left.attackPower;
+            param.speed = right.speed + left.speed;
+
+            return param;
+        }
+
+        public static RiseParametor operator -(RiseParametor right, RiseParametor left)
+        {
+            var param = new RiseParametor();
+            param.attackPower = right.attackPower - left.attackPower;
+            param.speed = right.speed - left.speed;
+
+            return param;
         }
     }
 
     [SerializeField]
-    RiseParametor m_riseParam;  //ステータス上昇のパラメータ
+    RiseParametor m_riseParam = new RiseParametor(1.2f,1.2f);  //ステータス上昇のパラメータ
 
     [SerializeField]
     float m_time = 99.0f;
 
+    [SerializeField]
+    bool m_isAngerTimer = false;  //怒り状態をタイマー性にするかどうか
+
     bool m_isAnger = false;
+    ReactiveProperty<bool> m_isReactiveAnger = new ReactiveProperty<bool>(false);
 
     WaitTimer m_waitTimer;
+    StatusManagerBase m_statusManager;
 
     void Awake()
     {
         m_waitTimer = GetComponent<WaitTimer>();
+        m_statusManager = GetComponent<StatusManagerBase>();
+    }
+
+    private void Start()
+    {
+        m_isReactiveAnger.Skip(1).Subscribe(x => { UpdateBuffParametor(); }).AddTo(this);
     }
 
     public void StartAnger()
     {
         SetIsAnger(true);
 
-        m_waitTimer.AddWaitTimer(GetType(), m_time, EndAnger);
+        if (m_isAngerTimer) {
+            m_waitTimer.AddWaitTimer(GetType(), m_time, EndAnger);
+        }
     }
 
     void EndAnger()
     {
         SetIsAnger(false);
+    }
+
+    void UpdateBuffParametor()
+    {
+        var param = m_statusManager.GetBuffParametor();
+
+        if (m_isAnger) {
+            param.angerParam = param.angerParam + m_riseParam;
+        }
+        else {
+            param.angerParam = param.angerParam - m_riseParam;
+        }
+
+        m_statusManager.SetBuffParametor(param);
     }
 
     //アクセッサ--------------------------------------------------------
@@ -58,6 +104,7 @@ public class AngerManager : MonoBehaviour
     public void SetIsAnger(bool isAnger)
     {
         m_isAnger = isAnger;
+        m_isReactiveAnger.Value = isAnger;
     }
 
     public RiseParametor GetRiseParametor()
