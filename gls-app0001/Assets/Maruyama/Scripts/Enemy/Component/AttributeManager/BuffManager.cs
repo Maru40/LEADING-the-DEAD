1ae9c1746559e1ea307e92fcef185232fe10bab4
Maru <zeal20300058@fsgcl.jp>
2021-10-12 13:32:35 +0900

    2021WIZ-87
    BuffManagerの生成

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/AttributeManager/BuffManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct BuffParametor
+{
+    public AngerManager.RiseParametor angerParam;
+
+    public BuffParametor(AngerManager.RiseParametor angerParam)
+    {
+        this.angerParam = angerParam;
+    }
+}
+
+public class BuffManager
+{
+    BuffParametor m_param = new BuffParametor();
+
+    public BuffManager()
+        :this(new BuffParametor(new AngerManager.RiseParametor(1.0f,1.0f)))
+    { }
+
+    public BuffManager(BuffParametor param)
+    {
+        m_param = param;
+    }
+
+    public void SetParametor(BuffParametor parametor)
+    {
+        m_param = parametor;
+    }
+    public BuffParametor GetParametor()
+    {
+        return m_param;
+    }
+}
