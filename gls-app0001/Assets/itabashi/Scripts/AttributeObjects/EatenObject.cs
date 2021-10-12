171cdd2b10c65f47cc86c7e9b55e84f8107f313c
Itabashi Oota <itaoo0430@gmail.com>
2021-10-12 16:37:01 +0900

    2021WIZ-92
    食べられる機能を持ったコンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/AttributeObjects/EatenObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class EatenObject : MonoBehaviour
+{
+    [SerializeField,Min(0)]
+    private float m_eatWeight = 1.0f;
+
+    public float eatWeight => m_eatWeight;
+
+    [SerializeField]
+    private UnityEvent<AttributeObject.DamageData> m_eatenEvent;
+
+    public void Eaten(in AttributeObject.DamageData eatenDamageData)
+    {
+        m_eatenEvent?.Invoke(eatenDamageData);
+    }
+}
