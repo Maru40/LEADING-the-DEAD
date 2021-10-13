74236311f049e721be6514680bcad4cefad1558f
Maru <zeal20300058@fsgcl.jp>
2021-10-13 11:47:06 +0900

    2021WIZ-88
    StatusManager_ZombieTankでEatenDamageで食べられるようにした。

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/Standard/Zombie/Tank/Status/StatusManager_ZombieTank.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/Standard/Zombie/Tank/Status/StatusManager_ZombieTank.cs
@@ -1,17 +1,37 @@
-﻿using System.Collections;
+﻿using AttributeObject;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StatusManager_ZombieTank : StatusManagerBase
 {
+    WaitTimer m_waitTimer;
 
-    void Start()
+    void Awake()
     {
-        
+        m_waitTimer = GetComponent<WaitTimer>();
     }
 
-    void Update()
+    public override void Damage(DamageData data)
     {
-        
+        //if (m_waitTimer.IsWait(GetType())) {
+        //    return;
+        //}
+
+        //m_status.hp -= data.damageValue;
+
+        //if (m_status.hp <= 0) 
+        //{
+        //    m_status.hp = 0;
+        //    gameObject.SetActive(false);
+        //}
+
+        ////ダメージインターバル開始
+        //float time = m_status.damageIntervalTime;
+        //m_waitTimer.AddWaitTimer(GetType(), time);
+    }
+
+    public void EatDamage() {
+        gameObject.SetActive(false);
     }
 }
