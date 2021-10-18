58ec6cf32fa74e50a9e1df6ad8f78a210f0e83d1
Maru <zeal20300058@fsgcl.jp>
2021-10-18 17:17:34 +0900

    2021WIZ-91
    TagDamageで指定したタグの時のみダメージが通るように変更

---
+++ gls-app0001/Assets/Maruyama/Scripts/StageObject/TagDamage.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagDamage : MonoBehaviour
+{
+    [SerializeField]
+    List<string> m_tags = new List<string>();
+
+    [SerializeField]
+    GameObject m_particle = null;
+
+    public void Damaged(AttributeObject.DamageData data)
+    {
+        foreach(var tag in m_tags){
+            if (data.FindTag(tag))
+            {
+                DamageProcess();
+                break;
+            }
+        }
+    }
+
+    protected virtual void DamageProcess()
+    {
+        CreateParticle();
+
+        Destroy(gameObject, 0.1f);
+    }
+
+    protected virtual void CreateParticle()
+    {
+        if (m_particle) {
+            Instantiate(m_particle, transform.position, Quaternion.identity);
+        }
+    }
+}
