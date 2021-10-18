46277325ba3384da0e68cf17c95a32fa08299a88
Itabashi Oota <itaoo0430@gmail.com>
2021-10-18 12:40:04 +0900

    2021WIZ-107
    閃光弾を上げる銃のモデルアセットをインポート

---
+++ gls-app0001/Assets/itabashi/DownloadAssets/Flare Gun/Scripts/Flaregun.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flaregun : MonoBehaviour
+{
+
+	public Rigidbody flareBullet;
+	public Transform barrelEnd;
+	public GameObject muzzleParticles;
+	public AudioClip flareShotSound;
+
+	public int bulletSpeed = 2000;
+
+	public void Shoot()
+	{
+		GetComponent<Animation>().CrossFade("Shoot");
+		GetComponent<AudioSource>().PlayOneShot(flareShotSound);
+
+
+		Rigidbody bulletInstance;
+		bulletInstance = Instantiate(flareBullet, barrelEnd.position, barrelEnd.rotation) as Rigidbody; //INSTANTIATING THE FLARE PROJECTILE
+
+
+		bulletInstance.AddForce(barrelEnd.forward * bulletSpeed); //ADDING FORWARD FORCE TO THE FLARE PROJECTILE
+
+		Instantiate(muzzleParticles, barrelEnd.position, barrelEnd.rotation);   //INSTANTIATING THE GUN'S MUZZLE SPARKS	
+	}
+}
