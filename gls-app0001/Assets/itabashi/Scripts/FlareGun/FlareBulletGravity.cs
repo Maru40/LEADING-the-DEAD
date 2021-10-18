ad7364e59e854614fdb6d85c6d0e3d3c6361d210
Itabashi Oota <itaoo0430@gmail.com>
2021-10-18 15:22:38 +0900

    2021WIZ-107
    閃光弾が上がり切ったら停止するコンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/FlareGun/FlareBulletGravity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlareBulletGravity : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody m_rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_rigidbody.velocity.y < 0)
+        {
+            m_rigidbody.useGravity = false;
+            m_rigidbody.velocity = new Vector3();
+        }
+    }
+}
