194d384c8ea57a28cceed4388b51b9c8840af0e2
Maru <zeal20300058@fsgcl.jp>
2021-10-18 10:17:49 +0900

    2021WIZ-105
    MaruTesterの実装

---
+++ gls-app0001/Assets/Maruyama/Scripts/Tester/MaruTester.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaruTester : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TestEnter()
+    {
+        Debug.Log("Enter");
+    }
+
+    public void TestUpdate()
+    {
+        Debug.Log("Update");
+    }
+
+    public void TestExit()
+    {
+        Debug.Log("Exit");
+    }
+}
