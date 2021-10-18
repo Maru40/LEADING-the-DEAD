99421e25c6441318b91205b3b9d95024eef6ee79
Maru <zeal20300058@fsgcl.jp>
2021-10-18 11:21:31 +0900

    2021WIZ-105
    MyAnimationBehaviorでAnimationの設定をできるようにした。

--- gls-app0001/Assets/Maruyama/Scripts/Behavior/MyAnimationBehavior.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Behavior/MyAnimationBehavior.cs
@@ -8,7 +8,7 @@ using System;
 public class MyAnimationBehavior : StateMachineBehaviour
 {
     [Serializable]
-    struct AnimatorTimerParametor
+    class AnimatorTimerParametor
     {
         public float time;
         public UnityEvent eventFunc;
@@ -21,6 +21,10 @@ public class MyAnimationBehavior : StateMachineBehaviour
             this.isEnd = false;
         }
     }
+
+    [SerializeField]
+    List<AnimatorTimerParametor> m_timerParam = new List<AnimatorTimerParametor>();
+    float m_beforeTime = 0.0f;
 
     [SerializeField]
     UnityEvent m_enterEvent = null;
@@ -40,6 +44,44 @@ public class MyAnimationBehavior : StateMachineBehaviour
 
     public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        var animeTime = stateInfo.normalizedTime % 1.0f;
+
+        PlayAnimationEvent(animeTime);
+
+        if(m_beforeTime > animeTime) //前フレームの方が小さかったら再生が最初に戻る。
+        {
+            ReturnAnimation();    
+        }
+        m_beforeTime = animeTime;
+    }
+
+    /// <summary>
+    /// アニメーションのイベント再生
+    /// </summary>
+    /// <param name="time">現在の再生時間</param>
+    void PlayAnimationEvent(float time)
+    {
+        foreach(var param in m_timerParam)
+        {
+            if (param.isEnd) {
+                continue;
+            }
+
+            if(param.time <= time) {
+                param.eventFunc?.Invoke();
+                param.isEnd = true;
+            }
+        }
+    }
+
+    /// <summary>
+    /// アニメーションのループが入った時
+    /// </summary>
+    void ReturnAnimation()
+    {
+        foreach(var param in m_timerParam)
+        {
+            param.isEnd = false;
+        }
     }
 }
