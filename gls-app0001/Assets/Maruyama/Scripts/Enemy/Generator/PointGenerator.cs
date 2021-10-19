8d638c37d083ce84e610f14e5351ce1edb0c76e8
Maru <zeal20300058@fsgcl.jp>
2021-10-19 12:17:39 +0900

    2021WIZ-125
    PointGeneratorの生成

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Generator/PointGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointGenerator : GeneratorBase
+{
+    [SerializeField]
+    GameObject m_createObject = null;
+
+    [SerializeField]
+    List<GameObject> m_positionObjects = new List<GameObject>();
+
+    private void Start()
+    {
+        CreateObjects();
+    }
+    
+    void CreateObjects()
+    {
+        foreach(var obj in m_positionObjects)
+        {
+            var newObject = Instantiate(m_createObject, obj.transform.position, Quaternion.identity);
+            newObject.gameObject.SetActive(true);
+            newObject.transform.SetParent(this.transform);
+        }
+    }
+}
