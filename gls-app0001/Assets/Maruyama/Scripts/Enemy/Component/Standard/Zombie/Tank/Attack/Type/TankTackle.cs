4a1780efc5b7e5ee259dd021a166aa9db7cd067f
Maru <zeal20300058@fsgcl.jp>
2021-10-19 13:38:30 +0900

    2021WIZ-125
    TankTackleの生成

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/Standard/Zombie/Tank/Attack/Type/TankTackle.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MaruUtility;
+
+public class TankTackle : AttackBase
+{
+    enum State 
+    {
+        None,
+        Tackle,
+    }
+
+    State m_state = State.None;
+
+    TargetManager m_targetManager;
+    EnemyVelocityMgr m_velocityManager;
+
+    private void Awake()
+    {
+        m_targetManager = GetComponent<TargetManager>();
+        m_velocityManager = GetComponent<EnemyVelocityMgr>();
+    }
+
+    private void Update()
+    {
+        TackleMove();
+    }
+
+    void TackleMove()
+    {
+        if(m_state != State.Tackle) {  //Tackleでなかったら処理をしない
+            return;
+        }
+
+
+    }
+
+    public override void AttackStart()
+    {
+
+    }
+
+    public override void Attack()
+    {
+
+    }
+
+    public override void AttackHitEnd()
+    {
+
+    }
+
+    public override bool IsAttackStartRange()
+    {
+        float range = GetBaseParam().startRange;
+        FoundObject target = m_targetManager.GetNowTarget();
+        if (target) {
+            return Calculation.IsRange(gameObject, target.gameObject, range);
+        }
+        else {
+            return false;
+        }
+    }
+
+    public override void EndAnimationEvent()
+    {
+
+    }
+}
