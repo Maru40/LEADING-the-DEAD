c05cc646f0ceb275762842146037409e2113108f
Maru <zeal20300058@fsgcl.jp>
2021-10-22 12:18:37 +0900

    2021WIZ-139
    SignalGunPresenterでフィルアモートでクールタイムを表現できるようにした。

--- gls-app0001/Assets/Maruyama/Scripts/UI/SignalGunPresenter.cs
+++ gls-app0001/Assets/Maruyama/Scripts/UI/SignalGunPresenter.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-using UnityEngine.Events;
 using UniRx;
 
 public class SignalGunPresenter : MonoBehaviour
@@ -14,19 +14,27 @@ public class SignalGunPresenter : MonoBehaviour
     UseItemUI m_useItemUI = null;
 
     [SerializeField]
-    GameObject m_additiveImage = null;
+    Image m_additiveImage = null;
 
     void Start()
     {
         var coolTimeParcentage = m_signalGunCtrl.coolTimePercentageOnChanged;
 
         coolTimeParcentage.Where(parsent => parsent >= 1.0f)
-            .Subscribe(_ => { m_useItemUI.isValidity = true; }  )
+            .Subscribe(_ => { m_useItemUI.isValidity = true;
+                m_additiveImage.gameObject.SetActive(false); 
+            }  )
             .AddTo(this);
 
         coolTimeParcentage.Where(parsent => parsent <= 1.0f && m_useItemUI.isValidity == true)
             .Skip(1)
-            .Subscribe(_ => m_useItemUI.isValidity = false)
+            .Subscribe(_ => { m_useItemUI.isValidity = false;
+                m_additiveImage.gameObject.SetActive(true);
+            })
+            .AddTo(this);
+
+        coolTimeParcentage.Where(parsent => parsent <= 1.0f)
+            .Subscribe(parsent => m_additiveImage.fillAmount = parsent)
             .AddTo(this);
     }
 
