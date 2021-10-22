b5c094d7e39ae5b0956eaf50bcfe78635ebad08a
Maru <zeal20300058@fsgcl.jp>
2021-10-22 10:41:45 +0900

    2021WIZ-139
    SignalGunPresenterでクールタイムに合わせて、表示を切り替えられるようにした。

---
+++ gls-app0001/Assets/Maruyama/Scripts/UI/SignalGunPresenter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events;
+using UniRx;
+
+public class SignalGunPresenter : MonoBehaviour
+{
+    [SerializeField]
+    SignalGunCtrl m_signalGunCtrl = null;
+
+    [SerializeField]
+    UseItemUI m_useItemUI = null;
+
+    [SerializeField]
+    GameObject m_additiveImage = null;
+
+    void Start()
+    {
+        var coolTimeParcentage = m_signalGunCtrl.coolTimePercentageOnChanged;
+
+        coolTimeParcentage.Where(parsent => parsent >= 1.0f)
+            .Subscribe(_ => { m_useItemUI.isValidity = true; }  )
+            .AddTo(this);
+
+        coolTimeParcentage.Where(parsent => parsent <= 1.0f && m_useItemUI.isValidity == true)
+            .Skip(1)
+            .Subscribe(_ => m_useItemUI.isValidity = false)
+            .AddTo(this);
+    }
+
+}
