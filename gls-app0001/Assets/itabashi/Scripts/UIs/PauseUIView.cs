c18e769ae551f13e9b9e1eb5b2884b1bc7013157
Itabashi Oota <itaoo0430@gmail.com>
2021-10-22 15:10:46 +0900

    2021WIZ-147
    ポーズUIにボタンの参照を追加

--- gls-app0001/Assets/itabashi/Scripts/UIs/PauseUIView.cs
+++ gls-app0001/Assets/itabashi/Scripts/UIs/PauseUIView.cs
@@ -1,9 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class PauseUIView : MonoBehaviour
 {
+    [SerializeField]
+    private Button m_backGameButton;
+
+    [SerializeField]
+    private Button m_goStageSelectBotton;
+
+    private void OnEnable()
+    {
+        EventSystem.current.SetSelectedGameObject(m_backGameButton.gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
