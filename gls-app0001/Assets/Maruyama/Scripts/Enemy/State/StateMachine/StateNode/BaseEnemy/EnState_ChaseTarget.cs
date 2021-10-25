ff93048530be9d147beb6d4fb5bf91c0caa4815c
Maru <zeal20300058@fsgcl.jp>
2021-10-25 10:04:08 +0900

    2021WIZ-152
    AttackNodeManagerBaseに変更したことによる変更

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/State/StateMachine/StateNode/BaseEnemy/EnState_ChaseTarget.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/State/StateMachine/StateNode/BaseEnemy/EnState_ChaseTarget.cs
@@ -25,7 +25,7 @@ public class EnState_ChaseTarget : EnemyStateNodeBase<EnemyBase>
 
         var owner = GetOwner();
 
-        m_attackComp = owner.GetComponent<AttackBase>();
+        m_attackComp = owner.GetComponent<AttackNodeManagerBase>();
 
         var chaseTarget = owner.GetComponent<ChaseTarget>();
 
