e94b4fed112618410712d69b8231bb17f73566f3
Itabashi Oota <itaoo0430@gmail.com>
2021-10-25 16:06:21 +0900

    2021WIZ-108
    選択可能なものを表示するUIを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/UIs/PossibleUI.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.EventSystems;
+
+public class PossibleUI : MonoBehaviour
+{
+    class PossibleEvent
+    {
+        public string displayName;
+        public Action selectEvent;
+        public int instanceID;
+
+        public PossibleEvent(string displayName,Action selectEvent,int instanceID)
+        {
+            this.displayName = displayName;
+            this.selectEvent = selectEvent;
+            this.instanceID = instanceID;
+        }
+
+        public PossibleEvent(string displayName,int instanceID)
+        {
+            this.displayName = displayName;
+            this.instanceID = instanceID;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            PossibleEvent possibleEvent = obj as PossibleEvent;
+
+            if(possibleEvent == null)
+            {
+                return false;
+            }
+
+            return displayName == possibleEvent.displayName &&
+                instanceID == possibleEvent.instanceID;
+        }
+    }
+
+    private List<PossibleEvent> m_possibleEvents = new List<PossibleEvent>();
+
+    [SerializeField]
+    private Button m_button = null;
+
+    [SerializeField]
+    private Text m_text = null;
+
+    private int m_index = 0;
+
+    private void Awake()
+    {
+        UpdateSelect();
+    }
+
+    public void AddSelectPossible(string displayName,Action selectEvent,int instanceID)
+    {
+        m_possibleEvents.Add(new PossibleEvent(displayName, selectEvent, instanceID));
+
+        UpdateSelect();
+    }
+
+    public void RemoveSelectPossible(string displayName,int instanceID)
+    {
+        m_possibleEvents.Remove(new PossibleEvent(displayName, instanceID));
+
+        UpdateSelect();
+    }
+
+    public void UpdateSelect()
+    {
+        if (m_possibleEvents.Count == 0)
+        {
+            m_index = 0;
+            m_button.gameObject.SetActive(false);
+            return;
+        }
+
+        m_button.gameObject.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(m_button.gameObject);
+
+        m_index = Mathf.Clamp(m_index, 0, m_possibleEvents.Count - 1);
+
+        var selectPossible = m_possibleEvents[m_index];
+
+        m_text.text = selectPossible.displayName;
+    }
+
+    public void PushSelectEvent()
+    {
+        m_possibleEvents[m_index]?.selectEvent();
+    }
+}
