1de63de54b93cfa7f18e2bc33f607885379c7d31
Itabashi Oota <itaoo0430@gmail.com>
2021-10-27 15:13:37 +0900

    2021WIZ-99
    サウンド調整UIとオーディオミキサーをつなぐコンポーネントの作成

---
+++ gls-app0001/Assets/itabashi/Scripts/UIs/AudioOptionUIPresenter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class AudioOptionUIPresenter : MonoBehaviour
+{
+    [SerializeField]
+    private Slider m_bgmSlider = null;
+
+    [SerializeField]
+    private Slider m_seSlider = null;
+
+    [SerializeField]
+    private Button m_backButton = null;
+
+    [SerializeField]
+    private AudioOptioner m_audioOptioner = null;
+
+    private void Awake()
+    {
+        m_bgmSlider.onValueChanged.AsObservable()
+            .Subscribe(value => m_audioOptioner.BGMVolume = value / m_bgmSlider.maxValue)
+            .AddTo(this);
+
+        m_seSlider.onValueChanged.AsObservable()
+            .Subscribe(value => m_audioOptioner.SEVolume = value / m_seSlider.maxValue)
+            .AddTo(this);
+
+        m_backButton.onClick.AsObservable()
+            .Subscribe(_ => GameFocusManager.PopFocus())
+            .AddTo(this);
+    }
+
+    public void IsFocus()
+    {
+        GameFocusManager.PushFocus(m_bgmSlider.gameObject);
+    }
+}
