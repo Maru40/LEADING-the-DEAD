33a7c2918c7e1b7e65bb3f8873b5d81586679321
Itabashi Oota <itaoo0430@gmail.com>
2021-10-27 15:12:27 +0900

    2021WIZ-99
    ボタンとスライダーの組み合わせで、ボタンを選択するとスライダーにフォーカスが移るコンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/UIs/SelectSliderUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using UnityEngine.EventSystems;
+
+
+public class SelectSliderUI : MonoBehaviour
+{
+    [SerializeField]
+    private Button m_button = null;
+
+    [SerializeField]
+    private Slider m_slider = null;
+
+    private void Awake()
+    {
+        m_button.OnClickAsObservable()
+            .Subscribe(_ =>
+            {
+                GameFocusManager.PushFocus(m_slider.gameObject);
+            })
+            .AddTo(this);
+    }
+}
