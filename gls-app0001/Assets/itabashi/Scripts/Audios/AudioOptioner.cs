2ed9e93c4bf59bf8a904e3c3eaa5fdbe1ddae530
Itabashi Oota <itaoo0430@gmail.com>
2021-10-27 15:10:58 +0900

    2021WIZ-99
    オーディオミキサーに簡単に音量調整ができるようなクラスを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/Audios/AudioOptioner.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioOptioner : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer m_audioMixer = null;
+
+    private const string MASTER_VOLUME_KEY = "MasterVolume";
+
+    private const string BGM_VOLUME_KEY = "BGMVolume";
+
+    private const string SE_VOLUME_KEY = "SEVolume";
+
+    private const float DB_DEFAULT_MIN = -80.0f;
+
+    private const float DB_DEFAULT_MAX = 20.0f;
+
+    public float MasterVolume
+    {
+        set => m_audioMixer.SetFloat(MASTER_VOLUME_KEY, Mathf.Clamp(LeapToDB(value), DB_DEFAULT_MIN, DB_DEFAULT_MAX));
+        get
+        {
+            m_audioMixer.GetFloat(MASTER_VOLUME_KEY, out float value);
+            return DBToLeap(value);
+        }
+    }
+
+    public float BGMVolume
+    {
+        set => m_audioMixer.SetFloat(BGM_VOLUME_KEY, Mathf.Clamp(LeapToDB(value), DB_DEFAULT_MIN, DB_DEFAULT_MAX));
+        get
+        {
+            m_audioMixer.GetFloat(BGM_VOLUME_KEY, out float value);
+            return DBToLeap(value);
+        }
+    }
+
+    public float SEVolume
+    {
+        set => m_audioMixer.SetFloat(SE_VOLUME_KEY, Mathf.Clamp(LeapToDB(value), DB_DEFAULT_MIN, DB_DEFAULT_MAX));
+        get
+        {
+            m_audioMixer.GetFloat(SE_VOLUME_KEY, out float value);
+            return DBToLeap(value);
+        }
+    }
+
+    static private float LeapToDB(float value)
+    {
+        Debug.Log(DB_DEFAULT_MIN - DB_DEFAULT_MIN * value);
+        return DB_DEFAULT_MIN - DB_DEFAULT_MIN * value;
+    }
+
+    static private float DBToLeap(float dB)
+    {
+        return (-DB_DEFAULT_MIN + dB) / -DB_DEFAULT_MIN;
+    }
+}
