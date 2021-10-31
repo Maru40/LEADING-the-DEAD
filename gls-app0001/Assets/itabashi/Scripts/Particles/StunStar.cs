cf1d00f711304cce9a560f18b200f1de78389aac
Itabashi Oota <itaoo0430@gmail.com>
2021-10-31 11:28:12 +0900

    2021WIZ-175
    スタンの星回転制御用のスクリプトを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/Particles/StunStar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StunStar : MonoBehaviour
+{
+    [SerializeField]
+    private Animation m_animation;
+
+    [SerializeField]
+    private List<ParticleSystem> m_particles;
+
+    private void Awake()
+    {
+        foreach (var particle in m_particles)
+        {
+            particle.gameObject.SetActive(false);
+        }
+    }
+
+    public void Play()
+    {
+        m_animation.Play();
+
+        foreach(var particle in m_particles)
+        {
+            particle.gameObject.SetActive(true);
+            particle.Play();
+        }
+    }
+
+    public void Stop()
+    {
+        m_animation.Stop();
+
+        foreach(var particle in m_particles)
+        {
+            particle.gameObject.SetActive(false);
+        }
+
+    }
+}
