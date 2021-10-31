602edf3867105e8810687e450cdc8f3c9200a962
Maru <zeal20300058@fsgcl.jp>
2021-11-01 00:43:01 +0900

    2021WIZ-178
    StateTableNode_FireDamageの生成(未完成)

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/Damage/StateTableNode_FireDamage.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateTableNode_FireDamage : StateTableNodeBase<EnemyBase>
+{
+    WaitTimer m_waitTimer;
+
+    GameObject m_createParticle;  //生成するparticle
+    DamageParticleManager m_particleManager; //particleマネージャ
+
+    float m_time = 0.0f;
+
+    public StateTableNode_FireDamage(EnemyBase owner, GameObject createParticle, float time = 3.0f)
+        :base(owner)
+    { 
+        m_waitTimer = owner.GetComponent<WaitTimer>();
+        m_particleManager = owner.GetComponent<DamageParticleManager>();
+
+        m_createParticle = createParticle;
+        m_time = time;
+    }
+
+    protected override void ReserveChangeComponents()
+    {
+
+    }
+
+    public override void OnStart()
+    {
+        base.OnStart();
+
+        m_waitTimer.AddWaitTimer(GetType(), m_time, End);  //指定時間後に終了関数を呼ぶ。
+        m_particleManager.StartDamage(m_time, m_createParticle);  //particleマネージャにparticleの生成依頼。
+    }
+
+    public override void OnUpdate()
+    {}
+
+    //アクセッサ---------------------------------------------------
+
+    public float Timer
+    {
+        set { m_time = value; }
+        get { return m_time; }
+    }
+}
