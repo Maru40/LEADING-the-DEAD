db1e24d48382b92bfe3086713687ad667aac726c
Itabashi Oota <itaoo0430@gmail.com>
2021-10-31 14:16:13 +0900

    2021WIZ-177
    パーティクルがすべて再生が終わった時にgameobjectをDestroyするコンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/Particles/ParticleStopDestroy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleStopDestroy : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem[] m_particleSystems;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(var particle in m_particleSystems)
+        {
+            if(!particle)
+            {
+                continue;
+            }
+
+            if(particle.isEmitting)
+            {
+                return;
+            }
+        }
+
+        Destroy(gameObject);
+    }
+}
