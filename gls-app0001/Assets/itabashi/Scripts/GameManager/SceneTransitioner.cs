ca98a85fe45680e6bfb39cdf7a7f2eb0c8857907
Itabashi Oota <itaoo0430@gmail.com>
2021-11-01 13:27:22 +0900

    2021WIZ-185
    簡易的にシーンを移動できるコンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/GameManager/SceneTransitioner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitioner : MonoBehaviour
+{
+    [SerializeField]
+    private SceneObject m_nextSceneObject;
+
+    public void GoNextScene()
+    {
+        GoNextScene(m_nextSceneObject);
+    }
+
+    public void GoNextScene(SceneObject sceneObject)
+    {
+        SceneManager.LoadScene(sceneObject);
+    }
+}
