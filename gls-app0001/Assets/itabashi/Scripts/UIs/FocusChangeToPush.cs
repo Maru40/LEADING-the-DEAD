a03eb961dc854652196d57b090e00cc9db8b1fb1
Itabashi Oota <itaoo0430@gmail.com>
2021-11-01 14:46:24 +0900

    2021WIZ-185
    UIのポップアップ用のコンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/UIs/FocusChangeToPush.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FocusChangeToPush : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_pushActiveObject;
+
+    [SerializeField]
+    private Selectable m_moveSelectableObject;
+    
+    public void NextPushFocus()
+    {
+        m_pushActiveObject.SetActive(true);
+        GameFocusManager.PushFocus(m_moveSelectableObject.gameObject);
+    }
+}
