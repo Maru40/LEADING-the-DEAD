be5e065621478b71366c864a7a4ee451c6fad864
Itabashi Oota <itaoo0430@gmail.com>
2021-11-01 13:26:36 +0900

    2021WIZ-185
    タイトルの最初の選択UIを管理するコンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/UIs/TitleSelectUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TitleSelectUI : MonoBehaviour
+{
+    [SerializeField]
+    private Button m_gameStartButton;
+
+    [SerializeField]
+    private Button m_optionButton;
+
+    [SerializeField]
+    private Button m_exitButton;
+
+    private void Awake()
+    {
+        EventSystem.current.SetSelectedGameObject(m_gameStartButton.gameObject);
+    }
+}
