86d1b2ab557483d7acc0ec887f0939f3b3ac23ca
Itabashi Oota <itaoo0430@gmail.com>
2021-11-03 16:46:59 +0900

    2021WIZ-189
    マップ上をさすポインター用のコンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/UIs/StagePointer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StagePointer : MonoBehaviour
+{
+    [SerializeField]
+    private Image m_pointerImage;
+
+    [SerializeField]
+    private Text m_stageNameText;
+
+    private RectTransform m_rectTransform;
+
+    private void Awake()
+    {
+        m_rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnStageChanged(StageSelecter.SelectStageData selectStageData)
+    {
+        if(selectStageData.stageData == null)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+
+        gameObject.SetActive(true);
+
+        m_rectTransform.anchoredPosition = selectStageData.stageData.stagePoint;
+
+        m_stageNameText.text = selectStageData.stageData.StageName;
+    }
+}
