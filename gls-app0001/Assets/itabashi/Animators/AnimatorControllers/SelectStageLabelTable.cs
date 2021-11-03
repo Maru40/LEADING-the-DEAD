7a758870b079ffcb9986cc92aaaf1bfbcf40e6a1
Itabashi Oota <itaoo0430@gmail.com>
2021-11-03 16:56:33 +0900

    2021WIZ-189
    SelectStageLabelのアニメーションコントローラーを作成

---
+++ gls-app0001/Assets/itabashi/Animators/AnimatorControllers/SelectStageLabelTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class SelectStageLabelTable
+{
+    public static readonly BaseLayerTable BaseLayer = new BaseLayerTable();
+    public class BaseLayerTable
+    {
+        public readonly AnimationState Idle = new AnimationState("Base Layer.Idle","Base Layer");
+        public readonly AnimationState FadeOut = new AnimationState("Base Layer.FadeOut","Base Layer");
+        public readonly AnimationState FadeIn = new AnimationState("Base Layer.FadeIn","Base Layer");
+        
+    }
+    
+}
