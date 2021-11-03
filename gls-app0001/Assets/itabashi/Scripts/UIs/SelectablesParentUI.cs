2a2883bbb0e2f6229539d6c6fd416fa14b0d386d
Itabashi Oota <itaoo0430@gmail.com>
2021-11-03 10:23:45 +0900

    2021WIZ-188
    選択の親を持ったコンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/UIs/SelectablesParentUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SelectablesParentUI : Selectable
+{
+
+    [SerializeField]
+    private Selectable m_firstSelectable;
+
+    protected override void OnValidate()
+    {
+        base.OnValidate();
+
+        if(m_firstSelectable == null || m_firstSelectable == this)
+        {
+            m_firstSelectable = null;
+            return;
+        }
+
+        if(!transform.IsChildOf(m_firstSelectable.transform))
+        {
+            m_firstSelectable = null;
+        }
+    }
+
+    public override void OnSelect(BaseEventData eventData)
+    {
+        m_firstSelectable.OnSelect(eventData);
+        m_firstSelectable.Select();
+    }
+}
