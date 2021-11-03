d180eeb07d65e0193b33a9f3ca7c8318047f9868
Itabashi Oota <itaoo0430@gmail.com>
2021-11-03 10:24:16 +0900

    2021WIZ-188
    SlideUIのアニメーションを管理するコンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/UIs/SlideUIAnimatorManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class SlideUIAnimatorManager : MonoBehaviour
+{
+    [SerializeField]
+    private Animator m_animator;
+
+    [SerializeField]
+    private StageSelectSceneEvent m_sceneEvent;
+
+    private void Awake()
+    {
+        var baseLayer = SlideUITable.BaseLayer;
+
+        var idleBehaviour = baseLayer.Idle.GetBehaviour<TimeEventStateMachineBehaviour>(m_animator);
+
+        idleBehaviour.onStateEntered.Subscribe(_ => m_sceneEvent.enabled = true);
+
+        var leftScrollBehaviour = baseLayer.LeftScroll.LeftScrollOut.GetBehaviour<TimeEventStateMachineBehaviour>(m_animator);
+
+        leftScrollBehaviour.onStateEntered.Subscribe(_ => m_sceneEvent.enabled = false);
+
+        var rightScrollBehaviour = baseLayer.RightScroll.RihgtScrollOut.GetBehaviour<TimeEventStateMachineBehaviour>(m_animator);
+
+        rightScrollBehaviour.onStateEntered.Subscribe(_ => m_sceneEvent.enabled = false);
+    }
+
+    public void LeftScroll(StageSelecter.SelectStageData selectStageData)
+    {
+        m_animator.Play(SlideUITable.BaseLayer.LeftScroll.LeftScrollOut.stateFullPath);
+    }
+
+    public void RightScroll(StageSelecter.SelectStageData selectStageData)
+    {
+        m_animator.Play(SlideUITable.BaseLayer.RightScroll.RihgtScrollOut.stateFullPath);
+    }
+}
