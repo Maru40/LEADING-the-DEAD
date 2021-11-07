57a015d1b13c06d413ccb951ac92f1947f27e396
Itabashi Oota <itaoo0430@gmail.com>
2021-11-07 16:25:54 +0900

    2021WIZ-226
    シングルトンのオーディオ用クラスコンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/GameManager/GameAudioManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Manager
+{
+    public class GameAudioManager : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioSource m_bgmSource;
+
+        [SerializeField]
+        private AudioSource m_seSource;
+
+        public static GameAudioManager Instance { private set; get; }
+
+        private void Awake()
+        {
+            if(Instance == null)
+            {
+                Instance = this;
+            }
+        }
+
+        public void BGMPlay(AudioClip bgmClip)
+        {
+            if(m_bgmSource.isPlaying)
+            {
+                m_bgmSource.Stop();
+            }
+
+            m_bgmSource.clip = bgmClip;
+
+            m_bgmSource.Play();
+        }
+
+        public void BGMStop()
+        {
+            m_bgmSource.Stop();
+        }
+
+        public void BGMPause()
+        {
+            m_bgmSource.Pause();
+        }
+
+        public void BGMUnPause()
+        {
+            m_bgmSource.UnPause();
+        }
+
+        public void SEPlayOneShot(AudioClip seClip, float volumeScale)
+        {
+            m_seSource.PlayOneShot(seClip, volumeScale);
+        }
+    }
+}
