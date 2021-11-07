f928fe676099086173d0f70d12225406e5afb2a4
Itabashi Oota <itaoo0430@gmail.com>
2021-11-07 14:59:45 +0900

    2021WIZ-225
    ポップアップ用のUIのコンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/UIs/PopUpUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_firstSelectObject;
+
+    public void Awake()
+    {
+        if(m_firstSelectObject == null)
+        {
+            m_firstSelectObject = gameObject;
+        }
+    }
+
+    public void PopUp()
+    {
+        gameObject.SetActive(true);
+        GameFocusManager.PushFocus(m_firstSelectObject);
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
+        GameFocusManager.PopFocus();
+    }
+}
