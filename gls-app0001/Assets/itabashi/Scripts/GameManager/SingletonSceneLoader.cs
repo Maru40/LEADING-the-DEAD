f396ec54aa000985b72df1a8c4598aeb3e069ab2
Itabashi Oota <itaoo0430@gmail.com>
2021-11-07 16:27:51 +0900

    2021WIZ-226
    起動時にSingletonManagerSceneを追加するクラスを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/GameManager/SingletonSceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Manager
+{
+    public class SingletonSceneLoader
+    {
+        public const string m_loadSceneName = "SingletonManagerScene";
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void SingletonSceneLoad()
+        {
+            SceneManager.LoadScene(m_loadSceneName, LoadSceneMode.Additive);
+        }
+    }
+}
