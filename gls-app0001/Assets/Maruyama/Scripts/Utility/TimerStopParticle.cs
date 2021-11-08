94b75c56c4606a66cf6d69696da954d631a5187d
Maru <zeal20300058@fsgcl.jp>
2021-11-08 10:37:00 +0900

    2021WIZ-224
    TimerStopParticleの実装

---
+++ gls-app0001/Assets/Maruyama/Scripts/Utility/TimerStopParticle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerStopParticle : MonoBehaviour
+{
+    [Header("particleがストップする時間"), SerializeField]
+    float m_stopTime = 5.0f;
+    [Header("particleがストップしてから消滅するまでの時間"), SerializeField]
+    float m_destoryTime = 3.0f;
+
+    GameTimer m_stopTimer = new GameTimer();
+    GameTimer m_destoryTimer = new GameTimer();
+
+    ParticleSystem m_particle;
+
+    private void Awake()
+    {
+        m_particle = GetComponent<ParticleSystem>();
+    }
+
+    void Start()
+    {
+        m_stopTimer.ResetTimer(m_stopTime, ParticleStop);
+    }
+
+    void Update()
+    {
+        m_stopTimer.UpdateTimer();
+        m_destoryTimer.UpdateTimer();
+    }
+
+    private void ParticleStop()
+    {
+        m_particle.Stop();
+        m_destoryTimer.ResetTimer(m_destoryTime,() => Destroy(gameObject));
+    }
+}
