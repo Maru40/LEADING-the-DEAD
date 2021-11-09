f11bb83a2c388e0ac6114504087c79df2af99df6
Maru <zeal20300058@fsgcl.jp>
2021-11-09 11:13:18 +0900

    2021WIZ-248
    AnimatorCtrl_ZombieNormalでBaseSpeedを設定できる用のプロパティを作成

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/Standard/Zombie/Nomal/Animator/AnimatorCtrl_ZombieNormal.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/Standard/Zombie/Nomal/Animator/AnimatorCtrl_ZombieNormal.cs
@@ -6,6 +6,12 @@ using UnityEngine;
 public class AnimatorCtrl_ZombieNormal : MonoBehaviour
 {
     float m_defaultAttackSpeed = 1.0f;
+    float m_baseMoveSpeed = 1.0f;
+    public float BaseMoveSpeed
+    {
+        get => m_baseMoveSpeed;
+        set => m_baseMoveSpeed = value;
+    }
 
     Rigidbody m_rigid;
     Animator m_animator;
@@ -25,7 +31,7 @@ public class AnimatorCtrl_ZombieNormal : MonoBehaviour
     void Update()
     {
         //仮歩き同期
-        moveSpeed = m_rigid.velocity.magnitude;
+        moveSpeed = m_rigid.velocity.magnitude * BaseMoveSpeed;
     }
 
     //アクセッサ---------------------------------------------------
