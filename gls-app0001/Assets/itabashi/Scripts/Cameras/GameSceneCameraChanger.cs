3837334d0f7bf9a74e991923d46f1c5d6d73b4a4
Itabashi Oota <itaoo0430@gmail.com>
2021-11-09 18:05:25 +0900

    2021WIZ-230
    ゲームシーン中のカメラ管理クラスを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/Cameras/GameSceneCameraChanger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSceneCameraChanger : MonoBehaviour
+{
+    [SerializeField]
+    private Camera m_mainCamera = null;
+    [SerializeField]
+    private Camera m_overLookingCamera = null;
+
+    private void Awake()
+    {
+        Manager.GameCameraManager.current = m_overLookingCamera;
+    }
+
+    private void Start()
+    {
+        Debug.Log(Manager.GameCameraManager.current);
+    }
+
+    public void ChangeCamera()
+    {
+        if (m_mainCamera.enabled)
+        {
+            Manager.GameCameraManager.current = m_overLookingCamera;
+        }
+        else
+        {
+            Manager.GameCameraManager.current = m_mainCamera;
+        }
+    }
+
+}
