feb0626176241e814d980edde4a0fadb9df06459
Itabashi Oota <itaoo0430@gmail.com>
2021-11-10 16:06:06 +0900

    2021WIZ-231
    トリガーの範囲内に入ったらイベントを呼ぶコンポーネントの作成

---
+++ gls-app0001/Assets/itabashi/Scripts/Cameras/RendererVisibleChacker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UniRx;
+using UniRx.Triggers;
+
+public class RendererVisibleChacker : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_checkObject;
+
+    [SerializeField]
+    private UnityEvent<bool> m_visibleChangeEvent = new UnityEvent<bool>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == m_checkObject)
+        {
+            m_visibleChangeEvent?.Invoke(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == m_checkObject)
+        {
+            m_visibleChangeEvent?.Invoke(false);
+        }
+    }
+}
