7db1b73f7c19567e44328d04a2f73066c2ff5bb9
Itabashi Oota <itaoo0430@gmail.com>
2021-11-10 16:06:55 +0900

    2021WIZ-231
    バリケードゲージの表示非表示を管理するコンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/UIs/BarricadeGaugeVisibleManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+
+public class BarricadeGaugeVisibleManager : MonoBehaviour
+{
+    [SerializeField]
+    private CanvasGroup m_canvasGroup;
+
+    private BoolReactiveProperty m_isVisible = new BoolReactiveProperty(false);
+
+    public bool IsVisible { set => m_isVisible.Value = value; get => m_isVisible.Value; }
+
+    private BoolReactiveProperty m_isChanging = new BoolReactiveProperty(false);
+
+    public bool IsChanging { set => m_isChanging.Value = value; get => m_isChanging.Value; }
+
+
+    public bool IsDraw => IsVisible || IsChanging;
+
+    private void Awake()
+    {
+        m_isVisible.Where(_ => IsDraw)
+            .Subscribe(_ => m_canvasGroup.alpha = 1.0f)
+            .AddTo(this);
+
+        m_isVisible.Where(_ => !IsDraw)
+            .Subscribe(_ => m_canvasGroup.alpha = 0.0f)
+            .AddTo(this);
+
+        m_isChanging.Where(_ => IsDraw)
+            .Subscribe(_ => m_canvasGroup.alpha = 1.0f)
+            .AddTo(this);
+
+        m_isChanging.Where(_ => !IsDraw)
+            .Subscribe(_ => m_canvasGroup.alpha = 0.0f)
+            .AddTo(this);
+    }
+}
