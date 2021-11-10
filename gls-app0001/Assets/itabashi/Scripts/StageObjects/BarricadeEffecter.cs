b4f38b9d0f9e3fbade51d1932f02d0340d884cb0
Itabashi Oota <itaoo0430@gmail.com>
2021-11-10 14:22:35 +0900

    2021WIZ-283
    バリケードのエフェクト用コンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/StageObjects/BarricadeEffecter.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarricadeEffecter : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem m_damageEffect;
+
+    [SerializeField]
+    private float m_probability = 0.6f;
+
+    [SerializeField]
+    private float m_maxY = 1.0f;
+
+    [SerializeField]
+    private float m_minY = -1.0f;
+
+    [SerializeField]
+    private float m_particleX = 3.85f;
+
+    [SerializeField, Min(0)]
+    private int m_breakParticleNum = 10;
+
+    private void CreateParticle()
+    {
+        float y = Random.Range(m_minY, m_maxY);
+
+        int i = Random.Range(0, 2);
+
+        Vector3 position = transform.position;
+        position.y += y;
+
+        if (i == 0)
+        {
+            position.x += m_particleX;
+        }
+        else
+        {
+            position.x -= m_particleX;
+        }
+
+        Instantiate(m_damageEffect, position, transform.rotation);
+    }
+
+    public void Damage()
+    {
+        if (m_probability == 0.0f || Random.Range(0.0f, 1.0f) <= m_probability)
+        {
+            return;
+        }
+
+        CreateParticle();
+    }
+
+    public void Break()
+    {
+        for (int i = 0; i < m_breakParticleNum; ++i)
+        {
+            CreateParticle();
+        }
+    }
+}
