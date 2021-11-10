6cd6b1a304d55360bdd8c99aa1753648b90be0a6
Maru <zeal20300058@fsgcl.jp>
2021-11-10 14:22:32 +0900

    2021WIZ-240
    FoundObjectで発見場所を乱数を持たせられるようにした。

--- gls-app0001/Assets/itabashi/Scripts/AttributeObjects/FoundObject.cs
+++ gls-app0001/Assets/itabashi/Scripts/AttributeObjects/FoundObject.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using MaruUtility;
+
 public class FoundObject : MonoBehaviour
 {
     public enum FoundType
@@ -16,12 +18,15 @@ public class FoundObject : MonoBehaviour
         private GameObject gameObject;
         public int priority;
         public FoundType type;
+        public Vector3 positionOffset;  //音などのポジションに乱数を持たせたい場合
+        public Vector3 position => gameObject.transform.position + positionOffset;
 
-        public FoundData(GameObject gameObject, int priority, FoundType type)
+        public FoundData(GameObject gameObject, int priority, FoundType type, Vector3 positionOffset)
         {
             this.gameObject = gameObject;
             this.priority = priority;
             this.type = type;
+            this.positionOffset = positionOffset;
         }
     }
 
@@ -31,8 +36,12 @@ public class FoundObject : MonoBehaviour
     [SerializeField]
     private FoundType m_type = FoundType.SoundObject;
 
+    [SerializeField]
+    private Vector3 m_positionOffset = new Vector3();
+
     public FoundData GetFoundData()
     {
-        return new FoundData(gameObject, m_priority, m_type);
+        var offset = RandomPosition.CalcuVec(m_positionOffset);
+        return new FoundData(gameObject, m_priority, m_type, offset);
     }
 }
