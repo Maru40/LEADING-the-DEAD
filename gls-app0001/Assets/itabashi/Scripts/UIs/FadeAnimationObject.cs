4997266b46c1e0bdc5170b4622e334ebe687ed1d
Itabashi Oota <itaoo0430@gmail.com>
2021-11-11 21:24:44 +0900

    2021WIZ-276
    アニメーションフェード用コンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/UIs/FadeAnimationObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SimpleAnimation))]
+
+public class FadeAnimationObject : FadeObject
+{
+    [SerializeField]
+    private SimpleAnimation m_animation;
+
+    private bool m_isStarted = false;
+
+    private void Reset()
+    {
+        m_animation = GetComponent<SimpleAnimation>();
+    }
+
+    public override void FadeStart()
+    {
+        m_animation.Play();
+        m_isStarted = true;
+    }
+
+    void Update()
+    {
+        Debug.Log(m_animation.isPlaying);
+    }
+
+    public override bool IsFinish()
+    {
+        return m_isStarted && !m_animation.isPlaying;
+    }
+}
