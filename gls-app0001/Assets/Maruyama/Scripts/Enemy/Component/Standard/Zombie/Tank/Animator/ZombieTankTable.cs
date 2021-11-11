aac183bdf4a3d75deecc115b10b64cf3e331bf94
Maru <zeal20300058@fsgcl.jp>
2021-11-12 01:48:45 +0900

    2021WIZ-251
    ZombieTankTableの生成

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/Standard/Zombie/Tank/Animator/ZombieTankTable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class ZombieTankTable
+{
+    public static readonly BaseLayerTable BaseLayer = new BaseLayerTable();
+    public class BaseLayerTable
+    {
+        public readonly AnimationState TackleAttack = new AnimationState("Base Layer.TackleAttack","Base Layer");
+        public readonly AnimationState Z_Idle = new AnimationState("Base Layer.Z_Idle","Base Layer");
+        public readonly AnimationState Z_Walk1 = new AnimationState("Base Layer.Z_Walk1","Base Layer");
+        public readonly AnimationState NormalAttack = new AnimationState("Base Layer.NormalAttack","Base Layer");
+        public readonly AnimationState TackleLast = new AnimationState("Base Layer.TackleLast","Base Layer");
+        
+    }
+    public static readonly UpperLayerTable UpperLayer = new UpperLayerTable();
+    public class UpperLayerTable
+    {
+        public readonly AnimationState Idle = new AnimationState("Upper Layer.Idle","Upper Layer");
+        public readonly AnimationState TackleCharge = new AnimationState("Upper Layer.TackleCharge","Upper Layer");
+        
+    }
+    
+}
