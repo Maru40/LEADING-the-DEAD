176e983a9bf1644e2cfc51de44915f1370d33462
Itabashi Oota <itaoo0430@gmail.com>
2021-11-12 12:13:45 +0900

    2021WIZ-232
    ライトを地面に飛ばし当たった場合に、当たり判定を出すように変更

--- gls-app0001/Assets/itabashi/Scripts/StageObjects/SearchLightManager.cs
+++ gls-app0001/Assets/itabashi/Scripts/StageObjects/SearchLightManager.cs
@@ -1,18 +1,32 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SearchLightManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private SphereCollider m_lightHitTrigger;
+
+    [SerializeField]
+    private Light m_light;
+
+    [SerializeField]
+    private LayerMask m_groundLayer;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hitInfo, m_light.range, m_groundLayer))
+        {
+            m_lightHitTrigger.transform.position = hitInfo.point;
+
+            m_lightHitTrigger.enabled = true;
+
+            m_lightHitTrigger.radius = Mathf.Tan(Mathf.Deg2Rad * m_light.spotAngle * 0.5f) * hitInfo.distance;
+        }
+        else
+        {
+            m_lightHitTrigger.enabled = false;
+        }
     }
 }
