06aa5da7e7eced0b625c82c3d9848e6d0b0678bc
Maru <zeal20300058@fsgcl.jp>
2021-11-15 21:32:21 +0900

    2021WIZ-305
    DotTestの実装

---
+++ gls-app0001/Assets/Maruyama/Scripts/Tester/DotTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotTest : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_target = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_target)
+        {
+            var toVec = m_target.transform.position - transform.position;
+            var testDot = Vector3.Dot(toVec.normalized, transform.forward);
+            Debug.Log("DotTest: " + testDot );
+        }
+    }
+}
