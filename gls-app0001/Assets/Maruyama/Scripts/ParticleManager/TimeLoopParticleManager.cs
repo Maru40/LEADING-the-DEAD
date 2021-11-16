5b852b668b39b940a83e3e84e903db4b716136d8
Maru <zeal20300058@fsgcl.jp>
2021-11-17 06:40:40 +0900

    2021WIZ-321
    TimeLoopParticleManagerの実装

---
+++ gls-app0001/Assets/Maruyama/Scripts/ParticleManager/TimeLoopParticleManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeLoopParticleManager : MonoBehaviour
+{
+    [SerializeField]
+    float m_intervalTime = 1.0f;
+
+    [SerializeField]
+    ParticleManager.ParticleID m_particleID;
+    [SerializeField]
+    Vector3 m_paticleSize = Vector3.one;
+
+    bool m_isActive = false;
+    GameTimer m_timer = new GameTimer();
+
+    void Update()
+    {
+        if (m_isActive == false) {
+            return;
+        }
+
+        m_timer.UpdateTimer();
+
+        if (m_timer.IsTimeUp)
+        {
+            ParticleManager.Instance.Play(m_particleID, transform.position, m_paticleSize);
+            m_timer.ResetTimer(m_intervalTime);
+        }
+    }
+
+    public void TimerStart()
+    {
+        m_isActive = true;
+
+        m_timer.ResetTimer(m_intervalTime);
+    }
+}
