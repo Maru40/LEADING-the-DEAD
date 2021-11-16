fdb05df1722db867464c146285f3f38905b8a85d
Itabashi Oota <itaoo0430@gmail.com>
2021-11-16 10:59:28 +0900

    2021WIZ-311
    UISounderにAudioSourceがついていなかった時、GameAudioManagerで再生するように変更

--- gls-app0001/Assets/itabashi/Scripts/Audios/UISounder.cs
+++ gls-app0001/Assets/itabashi/Scripts/Audios/UISounder.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using Manager;
 
 public class UISounder : MonoBehaviour, ISelectHandler, ISubmitHandler, ICancelHandler
 {
@@ -17,12 +18,22 @@ public class UISounder : MonoBehaviour, ISelectHandler, ISubmitHandler, ICancelH
     [SerializeField, CustomLabel("選択音")]
     private AudioClip m_selectSound;
 
+    [SerializeField]
+    private float m_seVolumeScale = 1.0f;
+
     private void OneShot(AudioClip audioClip)
     {
-        if(m_audioSource && audioClip)
+        if(!audioClip)
         {
-            m_audioSource.PlayOneShot(audioClip);
+            return;
         }
+
+        if(m_audioSource)
+        {
+            m_audioSource.PlayOneShot(audioClip, m_seVolumeScale);
+        }
+
+        GameAudioManager.Instance.SEPlayOneShot(audioClip, m_seVolumeScale);
     }
 
     public void SubmitPlay() => OneShot(m_submitSound);
