8cffa25bb93e5f7a83cb45e71f5a192462d3e670
Maru <zeal20300058@fsgcl.jp>
2021-11-17 10:43:58 +0900

    2021WIZ-321
    SmellManagerの実装

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/SmellManager/SmellManaer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmellManaer : MonoBehaviour
+{
+    TargetManager m_targetManager;
+    I_Smell m_smell;
+
+    private void Awake()
+    {
+        m_targetManager = GetComponent<TargetManager>();
+        m_smell = GetComponent<I_Smell>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var foundObject = other.GetComponent<FoundObject>();
+        if(foundObject)
+        {
+            if(foundObject.GetFoundData().type == FoundObject.FoundType.Smell)
+            {
+                m_smell?.SmellFind(foundObject);
+            }
+        }
+    }
+}
