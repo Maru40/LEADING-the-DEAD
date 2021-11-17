b6a63f9157d70b9dc25e8c6dcd4c9935c283f02e
Maru <zeal20300058@fsgcl.jp>
2021-11-17 10:44:15 +0900

    2021WIZ-321
    I＿Semllの実装

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Interface/I_Smell.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface I_Smell
+{
+    /// <summary>
+    /// 匂いに気づいた
+    /// </summary>
+    void SmellFind(FoundObject foundObject);  
+}
