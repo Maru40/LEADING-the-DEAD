c4c18df83b87a6e231b4ae5b23fb0a68d8ef9043
Maru <zeal20300058@fsgcl.jp>
2021-11-17 15:33:21 +0900

    2021WIZ-331
    BloodPuddleManagerの生成

---
+++ gls-app0001/Assets/Maruyama/Scripts/Item/BloodPuddle/BloodPuddleManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 血だまり管理
+/// </summary>
+public class BloodPuddleManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
