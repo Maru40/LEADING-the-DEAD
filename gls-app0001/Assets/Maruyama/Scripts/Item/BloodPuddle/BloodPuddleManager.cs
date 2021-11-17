27d4c59097a0be425a072be4741d78d1b34319de
Maru <zeal20300058@fsgcl.jp>
2021-11-18 02:37:26 +0900

    2021WIZ-331
    BloodPuddleManagerで血だまりの表現をできるようにした。

--- gls-app0001/Assets/Maruyama/Scripts/Item/BloodPuddle/BloodPuddleManager.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Item/BloodPuddle/BloodPuddleManager.cs
@@ -2,16 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using Es.InkPainter;
+
 /// <summary>
 /// 血だまり管理
 /// </summary>
 [RequireComponent(typeof(WaitTimer))]
 public class BloodPuddleManager : MonoBehaviour
 {
+    struct InkParametor
+    {
+        public InkCanvas ink;
+        public Brush brush;
+        public Vector3 position;
+
+        public InkParametor(InkCanvas ink, Brush brush, Vector3 position)
+        {
+            this.ink = ink;
+            this.brush = brush;
+            this.position = position;
+        }
+    }
+
     [SerializeField]
     float m_time = 5.0f;
 
     WaitTimer m_waitTimer;
+
+    List<InkParametor> m_inkParams = new List<InkParametor>();
 
     void Awake()
     {
@@ -28,7 +46,26 @@ public class BloodPuddleManager : MonoBehaviour
     /// </summary>
     void EndProcess()
     {
-        //将来的にフェード
+        InkErase();  //インクの消去
+
         Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// インクの消去
+    /// </summary>
+    void InkErase()
+    {
+        foreach (var param in m_inkParams)
+        {
+            param.ink.Erase(param.brush, param.position);
+        }
+    }
+
+    //アクセッサ・プロパティ----------------------------------------------------------
+
+    public void AddInk(InkCanvas ink, Brush brush, Vector3 position)
+    {
+        m_inkParams.Add(new InkParametor(ink, brush, position));
     }
 }
