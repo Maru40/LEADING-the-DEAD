8250b5c98fad8eeb3556325c559f12229245a462
Maru <zeal20300058@fsgcl.jp>
2021-11-17 16:13:01 +0900

    2021WIZ-331
    BloodPuddleManagerで制限時間を設けた。

--- gls-app0001/Assets/Maruyama/Scripts/Item/BloodPuddle/BloodPuddleManager.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Item/BloodPuddle/BloodPuddleManager.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 /// <summary>
 /// 血だまり管理
 /// </summary>
+[RequireComponent(typeof(WaitTimer))]
 public class BloodPuddleManager : MonoBehaviour
 {
-    void Start()
+    [SerializeField]
+    float m_time = 5.0f;
+
+    WaitTimer m_waitTimer;
+
+    void Awake()
     {
-        
+        m_waitTimer = GetComponent<WaitTimer>();
     }
 
-    void Update()
+    private void Start()
     {
-        
+        m_waitTimer.AddWaitTimer(GetType(), m_time, EndProcess);
+    }
+
+    /// <summary>
+    /// 終了関数。
+    /// </summary>
+    void EndProcess()
+    {
+        //将来的にフェード
+        Destroy(gameObject);
     }
 }
