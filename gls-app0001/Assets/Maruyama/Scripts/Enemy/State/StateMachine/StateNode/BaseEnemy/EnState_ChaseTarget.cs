718c72bf2a3c8d2c27a32b1c3daf78b2a60e3505
Maru <zeal20300058@fsgcl.jp>
2021-11-18 16:29:17 +0900

    2021WIZ-332
    EnState_Chaseでターゲットが攻撃対象なら攻撃するようにした。

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/State/StateMachine/StateNode/BaseEnemy/EnState_ChaseTarget.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/State/StateMachine/StateNode/BaseEnemy/EnState_ChaseTarget.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 using UnityEngine.AI;
 
+using FoundType = FoundObject.FoundType;
+
 public class EnState_ChaseTarget : EnemyStateNodeBase<EnemyBase>
 {
     AttackNodeManagerBase m_attackComp;
+    TargetManager m_targetManager;
 
     public EnState_ChaseTarget(EnemyBase owner)
         : base(owner)
-    { }
+    {
+        m_targetManager = owner.GetComponent<TargetManager>();
+    }
 
     protected override void ReserveChangeComponents()
     {
@@ -40,14 +45,71 @@ public class EnState_ChaseTarget : EnemyStateNodeBase<EnemyBase>
 
     public override void OnUpdate()
     {
+        StateCheck();
+
+        //if (m_attackComp.IsAttackStartRange())
+        //{
+        //    m_attackComp.AttackStart();
+        //}
+    }
+
+    public override void OnExit()
+    {
+        base.OnExit();
+    }
+
+    /// <summary>
+    /// ステートの遷移管理
+    /// </summary>
+    void StateCheck()
+    {
+        if(!m_targetManager.HasTarget()) {  //ターゲットがnullだったら
+            return; //処理をしない
+        }
+
+        var type = m_targetManager.GetNowTargetType();
+
+        System.Action action = type switch
+        {
+            FoundType.Player => PlayerCheck,
+            FoundType.Smell => SmellCheck,
+            _ => null,
+        };
+
+        action?.Invoke();
+    }
+
+    void PlayerCheck()
+    {
         if (m_attackComp.IsAttackStartRange())
         {
             m_attackComp.AttackStart();
         }
     }
 
-    public override void OnExit()
+    void SmellCheck()
     {
-        base.OnExit();
+        //現状はSmallManagerで管理中、後で習性候補
+        return;
+
+        //if (!m_targetManager.HasTarget()) {
+        //    return;
+        //}
+
+        //肉なら食べるモーション
+
+
+        //匂いならターゲットをnullにする。
+        //var positionCheck = m_targetManager.GetToNowTargetVector();
+        //if(positionCheck == null) {
+        //    return;
+        //}
+        //var toTargetVec = (Vector3)positionCheck;
+
+        //const float near = 0.5f;
+        //if(toTargetVec.magnitude < near)
+        //{
+        //    m_targetManager.AddExcludeNowTarget();
+        //}
     }
 }
