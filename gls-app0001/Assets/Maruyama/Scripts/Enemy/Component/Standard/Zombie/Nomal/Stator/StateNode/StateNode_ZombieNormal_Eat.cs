e347d4d282cf5c2c85a2c4005356b2a78ffd9ae9
Maru <zeal20300058@fsgcl.jp>
2021-11-19 15:52:57 +0900

    2021WIZ-323
    EatStateの実装

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/Standard/Zombie/Nomal/Stator/StateNode/StateNode_ZombieNormal_Eat.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateNode_ZombieNormal_Eat : EnemyStateNodeBase<EnemyBase>
+{
+    AnimatorManager_ZombieNormal m_animatorManager;
+    TargetManager m_targetManager;
+    Stator_ZombieNormal m_stator;
+    EnemyVelocityMgr m_velocityManager;
+
+    public StateNode_ZombieNormal_Eat(EnemyBase owner)
+        :base(owner)
+    {
+        m_animatorManager = owner.GetComponent<AnimatorManager_ZombieNormal>();
+        m_targetManager = owner.GetComponent<TargetManager>();
+        m_stator = owner.GetComponent<Stator_ZombieNormal>();
+        m_velocityManager = owner.GetComponent<EnemyVelocityMgr>();
+    }
+
+    protected override void ReserveChangeComponents()
+    {
+        
+    }
+
+    public override void OnStart()
+    {
+        base.OnStart();
+
+        m_animatorManager.CrossFadeEatAnimation();
+        m_velocityManager.StartDeseleration();
+    }
+
+    public override void OnUpdate()
+    {
+        Debug.Log("Eat");
+
+        if (m_targetManager.HasTarget())  //ターゲットが無かったら
+        {
+            var eaten = m_targetManager.GetNowTarget().GetComponent<EatenBase>();
+            if (eaten == null) {
+                m_stator.GetTransitionMember().rondomPlowlingTrigger.Fire();
+            }
+        }
+        else
+        {
+            m_stator.GetTransitionMember().rondomPlowlingTrigger.Fire();
+        }
+    }
+
+    public override void OnExit()
+    {
+        base.OnExit();
+
+        m_animatorManager.CrossFadeIdleAnimation(m_animatorManager.UpperLayerIndex);
+        m_velocityManager.SetIsDeseleration(false);
+    }
+}
