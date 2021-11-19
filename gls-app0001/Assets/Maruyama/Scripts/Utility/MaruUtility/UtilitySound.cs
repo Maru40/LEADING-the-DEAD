8578be2abfae2a3daf7786c159380ad1de9778a9
Maru <zeal20300058@fsgcl.jp>
2021-11-19 11:16:23 +0900

    2021WIZ-313
    UtilitySoundの生成

---
+++ gls-app0001/Assets/Maruyama/Scripts/Utility/MaruUtility/UtilitySound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MaruUtility
+{
+    namespace Sound
+    {
+        [System.Serializable]
+        struct AudioClipParametor
+        {
+            public AudioClip clip;
+            public float volume;
+
+            public AudioClipParametor(AudioClip clip, float volume)
+            {
+                this.clip = clip;
+                this.volume = volume;
+            }
+        }
+    }
+}
+
+
