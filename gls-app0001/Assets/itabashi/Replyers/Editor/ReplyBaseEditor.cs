b5b4f4c927b69bec7b612f95a789eb128efe30d1
Itabashi Oota <itaoo0430@gmail.com>
2021-11-24 11:36:24 +0900

    2021WIZ-414
    演出アニメーション用の基礎システムを作成

---
+++ gls-app0001/Assets/itabashi/Replyers/Editor/ReplyBaseEditor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Replyer.Editors
+{
+    [CustomEditor(typeof(ReplyerBase))]
+    public class ReplyBaseEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+        }
+
+        protected void EditBaseGUI()
+        {
+            serializedObject.Update();
+
+            var chance = serializedObject.FindProperty("m_chance");
+
+            chance.floatValue = EditorGUILayout.Slider(new GUIContent("発生確率"), chance.floatValue, 0.0f, 100.0f);
+
+            GUILayout.Space(20);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        public void DefaultPropertyField(SerializedObject serializedObject,string propertyPath)
+        {
+            var property = serializedObject.FindProperty(propertyPath);
+            EditorGUILayout.PropertyField(property);
+        }
+    }
+}
