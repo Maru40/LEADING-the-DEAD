0a433858c9dca76baa24ed66cfd8b81cba171c51
Itabashi Oota <itaoo0430@gmail.com>
2021-11-25 14:41:07 +0900

    2021WIZ-417
    CanvasGroupのAlphaを変更をアニメーションできるTrack系を作成

---
+++ gls-app0001/Assets/itabashi/Timelines/Scripts/CanvasGroupAlpha/CanvasGroupAlphaMixerBehabiour.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+using UnityEngine.UI;
+
+namespace Timelines.Playables.CanvasGroupAlpha
+{
+    public class CanvasGroupAlphaMixerBehabiour : PlayableBehaviour
+    {
+        public TimelineClip[] Clips { get; set; }
+        public PlayableDirector Director { get; set; }
+
+        public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+        {
+            var canvasGroup = playerData as CanvasGroup;
+
+            if(!canvasGroup)
+            {
+                return;
+            }
+
+            double time = Director.time;
+            float alpha = 0;
+
+            for (int i = 0; i < Clips.Length; ++i)
+            {
+                var clip = Clips[i];
+                var clipAsset = clip.asset as CanvasGroupAlphaClip;
+                var clipWeight = playable.GetInputWeight(i);
+                var clipProgress = (float)((time - clip.start) / clip.duration);
+
+                if (clipProgress >= 0.0f && clipProgress <= 1.0f)
+                {
+                    alpha += clipAsset.behaviour.alphaCurve.Evaluate(clipProgress) * clipWeight;
+                }
+            }
+
+            canvasGroup.alpha = alpha;
+        }
+    }
+}
---
+++ gls-app0001/Assets/itabashi/Timelines/Scripts/CanvasGroupAlpha/CanvasGroupAlphaTrack.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+using Timelines.Playables.CanvasGroupAlpha;
+
+namespace Timelines.Playables.Itabashi
+{
+    [TrackBindingType(typeof(CanvasGroup))]
+    [TrackColor(0,1,0)]
+    [TrackClipType(typeof(CanvasGroupAlphaClip))]
+    public class CanvasGroupAlphaTrack : TrackAsset
+    {
+        public override Playable CreateTrackMixer(PlayableGraph graph, GameObject go, int inputCount)
+        {
+            var mixer = ScriptPlayable<CanvasGroupAlphaMixerBehabiour>.Create(graph, inputCount);
+            mixer.GetBehaviour().Clips = GetClips().ToArray();
+            mixer.GetBehaviour().Director = go.GetComponent<PlayableDirector>();
+
+            return mixer;
+        }
+
+        public override void GatherProperties(PlayableDirector director, IPropertyCollector driver)
+        {
+#if UNITY_EDITOR
+            var trackBinding = director.GetGenericBinding(this) as CanvasGroup;
+            
+            if(!trackBinding)
+            {
+                return;
+            }
+
+            driver.AddFromName<CanvasGroup>(trackBinding.gameObject, "m_Alpha");
+#endif
+        }
+    }
+}
