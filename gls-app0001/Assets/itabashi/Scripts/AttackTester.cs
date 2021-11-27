4381e3876e0ca68c5cd7e44eaa3ea517af420828
Itabashi Oota <itaoo0430@gmail.com>
2021-11-27 17:27:08 +0900

    2021WIZ-417
    テスト用コンポーネントを更新

--- gls-app0001/Assets/itabashi/Scripts/AttackTester.cs
+++ gls-app0001/Assets/itabashi/Scripts/AttackTester.cs
@@ -1,8 +1,13 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackTester : MonoBehaviour
+public interface Test
+{
+    void A();
+}
+
+public class AttackTester : MonoBehaviour, Test
 {
     [SerializeField]
     private AudioOptioner audioOptioner;
@@ -10,13 +15,17 @@ public class AttackTester : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void A()
+    {
+        Debug.Log("成功");
     }
 
     private void OnCollisionEnter(Collision collision)
