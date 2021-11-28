b7b0954289a6ed2c1268c306aa80f1a225e5625a
Itabashi Oota <itaoo0430@gmail.com>
2021-11-28 17:58:51 +0900

    2021WIZ-421
    ポップアップUIをポップアップされたときと終了時のアニメーションを変えられるように変更

--- gls-app0001/Assets/itabashi/Scripts/UIs/PopUpUI.cs
+++ gls-app0001/Assets/itabashi/Scripts/UIs/PopUpUI.cs
@@ -1,15 +1,30 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.Playables;
+using UniRx;
 
 public class PopUpUI : MonoBehaviour
 {
+    public enum DirectorType
+    {
+        None,
+        Timeline,
+        SimpleStartEndAnimator,
+    }
+
     [SerializeField]
     public GameObject firstSelectObject;
 
     [SerializeField]
+    private DirectorType m_directorType = DirectorType.None;
+
+    [SerializeField]
     private PlayableDirector m_director;
+
+    [SerializeField]
+    private SimpleStartEndAnimateDirector m_animateDirector;
 
     public void Awake()
     {
@@ -18,9 +33,34 @@ public class PopUpUI : MonoBehaviour
             firstSelectObject = gameObject;
         }
 
-        if (m_director)
+        if(m_directorType == DirectorType.None)
+        {
+            return;
+        }
+
+        if (m_directorType == DirectorType.Timeline && m_director)
         {
             m_director.stopped += _ => GameFocusManager.PushFocus(firstSelectObject);
+            return;
+        }
+
+        if(m_directorType == DirectorType.SimpleStartEndAnimator && m_animateDirector)
+        {
+            m_animateDirector.finished
+                .Where(animationType => animationType == SimpleStartEndAnimateDirector.AnimationType.StartAnimation)
+                .Subscribe(_ => GameFocusManager.PushFocus(firstSelectObject))
+                .AddTo(this);
+
+            m_animateDirector.finished
+                .Where(animationType => animationType == SimpleStartEndAnimateDirector.AnimationType.EndAnimation)
+                .Subscribe(_ =>
+                {
+                    gameObject.SetActive(false);
+                    GameFocusManager.PopFocus();
+                })
+                .AddTo(this);
+
+            return;
         }
     }
 
@@ -28,16 +68,34 @@ public class PopUpUI : MonoBehaviour
     {
         gameObject.SetActive(true);
 
-        if (!m_director)
+        if (m_directorType == DirectorType.None)
         {
+            Debug.Log("ここから");
             GameFocusManager.PushFocus(firstSelectObject);
             return;
+        }
+
+        if(m_directorType == DirectorType.SimpleStartEndAnimator)
+        {
+            Debug.Log("ポップアップ");
+            m_animateDirector.StartAnimationPlay();
         }
     }
 
     public void Close()
     {
-        gameObject.SetActive(false);
-        GameFocusManager.PopFocus();
+
+        if (m_directorType == DirectorType.None)
+        {
+            gameObject.SetActive(false);
+            GameFocusManager.PopFocus();
+            return;
+        }
+
+        if (m_directorType == DirectorType.SimpleStartEndAnimator)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            m_animateDirector.EndAnimationPlay();
+        }
     }
 }
