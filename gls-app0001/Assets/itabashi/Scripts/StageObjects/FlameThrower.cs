198ea8e58f15e378f2775e16516ecdee0b2b8538
Itabashi Oota <itaoo0430@gmail.com>
2021-11-28 15:32:43 +0900

    2021WIZ-420
    火炎放射器のエフェクト戸当たり判定が合うように変更

--- gls-app0001/Assets/itabashi/Scripts/StageObjects/FlameThrower.cs
+++ gls-app0001/Assets/itabashi/Scripts/StageObjects/FlameThrower.cs
@@ -11,7 +11,7 @@ public class FlameThrower : MonoBehaviour
     private ParticleSystem m_flameParticle;
 
     [SerializeField]
-    private Collider m_flameDamageTrigger;
+    private CapsuleCollider m_flameDamageTrigger;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +21,7 @@ public class FlameThrower : MonoBehaviour
         if (m_isShoot)
         {
             m_flameParticle.Play();
+            StartCoroutine(ShootStart());
         }
         else
         {
@@ -31,22 +32,70 @@ public class FlameThrower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void IsShootChange()
     {
         m_isShoot = !m_isShoot;
 
-        m_flameDamageTrigger.enabled = m_isShoot;
-
         if(m_isShoot)
         {
+            m_flameDamageTrigger.enabled = true;
             m_flameParticle.Play();
+            StartCoroutine(ShootStart());
         }
         else
         {
             m_flameParticle.Stop();
+            StartCoroutine(ShootEnd());
         }
+    }
+
+    private IEnumerator ShootStart()
+    {
+        float countTime = 0.0f;
+
+        while (countTime < m_flameParticle.main.startLifetime.constant)
+        {
+            countTime += Time.deltaTime;
+
+            float height = m_flameParticle.main.startSpeed.constant * countTime;
+
+            m_flameDamageTrigger.height = height;
+
+            m_flameDamageTrigger.center = new Vector3(0, 0, height * 0.5f);
+
+            yield return null;
+        }
+
+        m_flameDamageTrigger.height = m_flameParticle.main.startSpeed.constant * m_flameParticle.main.startLifetime.constant;
+
+        m_flameDamageTrigger.center = new Vector3(0, 0, m_flameDamageTrigger.height * 0.5f);
+    }
+
+    private IEnumerator ShootEnd()
+    {
+        float countTime = 0.0f;
+
+        float endHeight = m_flameParticle.main.startSpeed.constant * m_flameParticle.main.startLifetime.constant;
+
+        while (countTime < m_flameParticle.main.startLifetime.constant)
+        {
+            countTime += Time.deltaTime;
+
+            float height = endHeight - m_flameParticle.main.startSpeed.constant * countTime;
+
+            m_flameDamageTrigger.height = height;
+
+            m_flameDamageTrigger.center = new Vector3(0, 0, endHeight - height * 0.5f);
+
+            yield return null;
+        }
+
+        m_flameDamageTrigger.height = m_flameParticle.main.startSpeed.constant * m_flameParticle.main.startLifetime.constant;
+
+        m_flameDamageTrigger.center = new Vector3(0, 0, endHeight);
+
+        m_flameDamageTrigger.enabled = false;
     }
 }
