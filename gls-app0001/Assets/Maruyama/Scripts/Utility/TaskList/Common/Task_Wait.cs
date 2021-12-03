7e655e521d5198539002eb341c7d3a000d7052dc
Maru <zeal20300058@fsgcl.jp>
2021-12-03 15:17:21 +0900

    2021WIZ-426
    Waitタスクの生成

---
+++ gls-app0001/Assets/Maruyama/Scripts/Utility/TaskList/Common/Task_Wait.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+public class Task_Wait : TaskNodeBase
+{
+    struct Parametor
+    {
+        public float time;
+        public Action enter;
+        public Action update;
+        public Action exit;
+    }
+
+    Parametor m_param = new Parametor();
+
+    public override void OnEnter()
+    {
+
+    }
+
+    public override bool OnUpdate()
+    {
+        return true;
+    }
+
+    public override void OnExit()
+    {
+
+    }
+}
