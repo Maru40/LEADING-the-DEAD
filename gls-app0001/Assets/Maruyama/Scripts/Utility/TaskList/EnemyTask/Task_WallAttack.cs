27689f79a3823d93a72719e3bd3500b4fa8d4c08
Maru <zeal20300058@fsgcl.jp>
2021-12-03 15:16:31 +0900

    2021WIZ-426
    WallAttackタスクの生成

---
+++ gls-app0001/Assets/Maruyama/Scripts/Utility/TaskList/EnemyTask/Task_WallAttack.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+public class Task_WallAttack : TaskNodeBase<EnemyBase>
+{
+    struct Parametor
+    {
+        
+    }
+
+    GameTimer m_timer = new GameTimer();
+
+    public Task_WallAttack(EnemyBase owner)
+        :base(owner)
+    {
+
+    }
+
+    public override void OnEnter()
+    {
+        //アニメーションの変更
+
+    }
+
+    public override bool OnUpdate()
+    {
+        return true;
+    }
+
+    public override void OnExit()
+    {
+
+    }
+}
