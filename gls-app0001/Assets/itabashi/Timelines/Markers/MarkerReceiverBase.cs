031183665c0abe8c827464b4fbd99f81b189de87
Itabashi Oota <itaoo0430@gmail.com>
2021-12-04 16:17:35 +0900

    2021WIZ-465
    マーカー用の抽象クラスを作成

---
+++ gls-app0001/Assets/itabashi/Timelines/Markers/MarkerReceiverBase.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public abstract class MarkerReceiverBase : MonoBehaviour, INotificationReceiver
+{
+    public abstract void OnNotify(Playable origin, INotification notification, object context);
+}
