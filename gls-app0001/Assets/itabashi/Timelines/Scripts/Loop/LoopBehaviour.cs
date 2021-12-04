3ddeda4cb011fdb49858a1a422b27d94636c4b99
Itabashi Oota <itaoo0430@gmail.com>
2021-12-04 16:10:55 +0900

    2021WIZ-465
    タイムラインのループ用のトラックを作成

---
+++ gls-app0001/Assets/itabashi/Timelines/Scripts/Loop/LoopBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace Timelines.Playables.Loop
+{
+    public class LoopBehaviour : PlayableBehaviour
+    {
+        private PlayableDirector m_director;
+
+        private double m_startTime = 0.0f;
+
+        public override void OnPlayableCreate(Playable playable)
+        {
+            m_director = playable.GetGraph().GetResolver() as PlayableDirector;
+        }
+
+        public override void OnBehaviourPlay(Playable playable, FrameData info)
+        {
+            m_startTime = m_director.time;
+        }
+
+        public override void OnBehaviourPause(Playable playable, FrameData info)
+        {
+            if (m_director)
+            {
+                m_director.time = m_startTime;
+            }
+        }
+    }
+}
