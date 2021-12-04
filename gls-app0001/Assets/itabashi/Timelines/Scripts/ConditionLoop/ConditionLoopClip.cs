8f84a79d93ead00cd147daa7d8454086482e1acb
Itabashi Oota <itaoo0430@gmail.com>
2021-12-04 16:11:43 +0900

    2021WIZ-465
    タイムラインの条件付きループをするトラックを作成

---
+++ gls-app0001/Assets/itabashi/Timelines/Scripts/ConditionLoop/ConditionLoopClip.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace Timelines.Playables.ConditionLoop
+{
+    [System.Serializable]
+    public class ConditionLoopClip : PlayableAsset, ITimelineClipAsset
+    {
+        public ClipCaps clipCaps { get; }
+
+        public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+        {
+            var playable = ScriptPlayable<ConditionLoopBehaviour>.Create(graph);
+            ConditionLoopBehaviour behaviour = playable.GetBehaviour();
+
+            return playable;
+        }
+    }
+}
---
+++ gls-app0001/Assets/itabashi/Timelines/Scripts/ConditionLoop/ConditionLoopTrack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace Timelines.Playables.Itabashi
+{
+    [TrackBindingType(typeof(TimelineLoopBase))]
+    [TrackColor(255.0f / 255.0f, 20.0f / 255.0f, 147.0f / 255.0f)]
+    [TrackClipType(typeof(ConditionLoop.ConditionLoopClip))]
+    public class ConditionLoopTrack : TrackAsset
+    {
+    }
+}
