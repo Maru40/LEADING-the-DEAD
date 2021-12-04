7b26b43ffe2599b97798108840781b20a5e4b310
Itabashi Oota <itaoo0430@gmail.com>
2021-12-04 16:10:01 +0900

    2021WIZ-465
    baheviourに登録したイベントを呼ぶマーカーを作成

---
+++ gls-app0001/Assets/itabashi/Timelines/Markers/BehaviourEventMarker/BehaviourEventMarker.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class BehaviourEventMarker : Marker, INotification
+{
+    public PropertyName id => new PropertyName("BehaviourEventMarker");
+}
