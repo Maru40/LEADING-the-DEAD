6aae221ec319c96858fc9e3281bee59dbe27da1b
Itabashi Oota <itaoo0430@gmail.com>
2021-12-07 16:50:48 +0900

    2021WIZ-488
    テキスト表示用のトリガーコンポーネントの作成

---
+++ gls-app0001/Assets/itabashi/Scripts/TutorialTextTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialTextTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private BoxCollider m_boxCollider;
+
+    [SerializeField]
+    private Text m_text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(!m_boxCollider)
+        {
+            return;
+        }
+
+        Color color = Color.green;
+        color.a = 0.5f;
+        Gizmos.color = color;
+        Gizmos.DrawCube(m_boxCollider.center, m_boxCollider.size);
+    }
+}
