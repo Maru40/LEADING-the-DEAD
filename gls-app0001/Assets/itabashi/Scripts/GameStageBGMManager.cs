bb5042248ed48507e8abd13838462e4d2733137b
Itabashi Oota <itaoo0430@gmail.com>
2021-12-08 20:04:50 +0900

    2021WIZ-523
    ゲームステージのBGM管理コンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/GameStageBGMManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Manager;
+
+public class GameStageBGMManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip m_gameStageBGM;
+
+    [SerializeField]
+    private AudioClip m_clearResultBGM;
+
+    [SerializeField]
+    private float m_fadeTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!GameAudioManager.Instance.IsBGMPlaying)
+        {
+            GameAudioManager.Instance.BGMPlay(m_gameStageBGM, m_fadeTime);
+        }
+    }
+
+    public void Claer()
+    {
+        GameAudioManager.Instance.BGMPlay(m_clearResultBGM);
+    }
+
+    public void BGMStop()
+    {
+        GameAudioManager.Instance.BGMStop(m_fadeTime);
+    }
+}
