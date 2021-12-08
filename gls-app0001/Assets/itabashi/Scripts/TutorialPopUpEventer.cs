cde6593560d0da4ab710b8c67f02d9e4a83645a7
Itabashi Oota <itaoo0430@gmail.com>
2021-12-08 16:00:56 +0900

    2021WIZ-488
    チュートリアルポップアップ用のコンポーネントの更新

--- gls-app0001/Assets/itabashi/Scripts/TutorialPopUpEventer.cs
+++ gls-app0001/Assets/itabashi/Scripts/TutorialPopUpEventer.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialPopUpEventer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private PopUpUI m_popUpUI;
+
+    [SerializeField]
+    private Image m_changedImage;
+
+    [SerializeField]
+    private Text m_changedText;
+
+    [SerializeField]
+    private GameStateManager m_gameStageManager;
+
+    [SerializeField]
+    private Sprite m_sprite;
+
+    [SerializeField, TextArea(1, 4)]
+    private string m_text;
+
+    public void PopUpEvent()
     {
-        
+        m_gameStageManager.ChangeForcedPause();
+
+        m_changedImage.sprite = m_sprite;
+        m_changedText.text = m_text;
+
+        m_popUpUI.PopUp();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
