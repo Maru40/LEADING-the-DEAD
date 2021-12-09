2c8f207b74d7bf294b2f8b235ebcf57e94987ff3
Maru <zeal20300058@fsgcl.jp>
2021-12-09 13:42:44 +0900

    2021WIZ-530
    StateNode_ZombieTank_Noneで動かないようにした。

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/Standard/Zombie/Tank/Stator/StateNode/StateNode_ZombieTank_None.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateNode_ZombieTank_None : EnemyStateNodeBase<EnemyBase>
+{
+    public StateNode_ZombieTank_None(EnemyBase owner)
+        :base(owner)
+    { }
+
+    protected override void ReserveChangeComponents()
+    {
+        var owner = GetOwner();
+
+        AddChangeComp(owner.GetComponent<RandomPlowlingMove>(), false, true);
+    }
+
+    public override void OnUpdate()
+    {
+
+    }
+}
