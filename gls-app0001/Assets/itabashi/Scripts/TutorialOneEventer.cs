cbf23d9b6ca047f726837eba1541eb7315edbeb4
Itabashi Oota <itaoo0430@gmail.com>
2021-12-09 16:23:24 +0900

    2021WIZ-517
    一度だけイベントが呼ばれるコンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/TutorialOneEventer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class TutorialOneEventer : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent m_oneCallEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void CallEvent()
+    {
+        if(enabled)
+        {
+            m_oneCallEvent?.Invoke();
+            enabled = false;
+        }
+    }
+}
