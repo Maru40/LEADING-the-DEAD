c5fda3132027b238ecb4a41a186522cb4a1a3f62
Itabashi Oota <itaoo0430@gmail.com>
2021-12-10 11:58:48 +0900

    2021WIZ-550
    ミッション用コンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/Missions/ZombieDeathCountMissionChecker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieDeathCountMissionChecker : MissionCheckerBase<AllEnemyGeneratorManager>
+{
+    [SerializeField, Min(0)]
+    private int m_zombieDeathCountBoarder;
+
+    public override string GetDisctiptionText()
+    {
+        return m_descriptionFront + m_zombieDeathCountBoarder.ToString() + m_descriptionEnd;
+    }
+
+    public override bool IsMissionClear(AllEnemyGeneratorManager allEnemyGeneratorManager)
+    {
+        return allEnemyGeneratorManager.GetAllDeashCount() <= m_zombieDeathCountBoarder;
+    }
+}
