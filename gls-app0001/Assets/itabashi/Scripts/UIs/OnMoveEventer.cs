6066cd3c5684b7064ac11fd562675e27a81cfbd2
Itabashi Oota <itaoo0430@gmail.com>
2021-12-12 14:28:01 +0900

    2021WIZ-574
    OnMoveをインスペクタから呼び出せるようにするコンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/UIs/OnMoveEventer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class OnMoveEventer : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent<AxisEventData> m_OnMoveEvent;
+
+    public void OnMove(BaseEventData baseEventData)
+    {
+        if(baseEventData is AxisEventData axisEventData)
+        {
+            m_OnMoveEvent?.Invoke(axisEventData);
+        }
+    }
+}
