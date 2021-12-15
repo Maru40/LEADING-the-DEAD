4cd08d58525a09a9e7471a312aaacc0ad24378a2
Maru <zeal20300058@fsgcl.jp>
2021-12-15 17:36:28 +0900

    2021WIZ-603
    ClearManagerでバリケードの前に生成するようにした。

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/GameManager/ClearManager_Zombie.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/GameManager/ClearManager_Zombie.cs
@@ -15,6 +15,7 @@ public class ClearManager_Zombie : MonoBehaviour
     TargetManager m_targetManager;
     EnemyVelocityMgr m_velocityManager;
     EnemyRotationCtrl m_rotationController;
+    ThrongManager m_throngManager;
 
     BarricadeDurability m_barricade = null;
 
@@ -26,19 +27,28 @@ public class ClearManager_Zombie : MonoBehaviour
     [Header("音をランダムなタイミングで出す"), SerializeField]
     RandomRange m_audioPlayRange = new RandomRange(0.0f, 0.5f);
 
+    [Header("扉を開けた時にランダムに生成する位置"), SerializeField]
+    RandomRange m_positionRandomRange = new RandomRange(-1.0f, +1.0f);
+    [Header(""), SerializeField]
+    RandomRange m_directRandomRange = new RandomRange(-30.0f, 30.0f);
+
     AnimatorManager_ZombieNormal m_animatorManager = null;
     WaitTimer m_waitTimer = null;
 
     Vector3 m_moveDirect = Vector3.zero;
 
+    Stator_ZombieNormal m_stator;
+
     private void Awake()
     {
         m_targetManager = GetComponent<TargetManager>();
         m_velocityManager = GetComponent<EnemyVelocityMgr>();
+        m_throngManager = GetComponent<ThrongManager>();
 
         m_animatorManager = GetComponent<AnimatorManager_ZombieNormal>();
         m_rotationController = GetComponent<EnemyRotationCtrl>();
         m_waitTimer = GetComponent<WaitTimer>();
+        m_stator = GetComponent<Stator_ZombieNormal>();
 
         m_barricade = FindObjectOfType<BarricadeDurability>();
 
@@ -56,27 +66,22 @@ public class ClearManager_Zombie : MonoBehaviour
     public void ClearProcess()
     {
         enabled = true;
+        //m_stator.SetIsTransitionLock(true);
 
         m_targetManager.SetNowTarget(GetType(), null);
-        ChangeEnableBehabiours();
+        ChangeEnableBehabiours(); //使わないBehaviourをfalseにする。
 
-        m_animatorManager.CrossFadeIdleAnimation();
-        m_animatorManager.CrossFadeIdleAnimation(m_animatorManager.UpperLayerIndex);
-        m_animatorManager.CrossFadeIdleAnimation(m_animatorManager.AllLayerIndex);
+        CrossFadeAnimation(); //アニメーションの遷移
 
-        m_moveDirect = transform.forward;
-        if (m_barricade) {  //バリケードの方向に向かって走り出す。
-            m_moveDirect = m_barricade.transform.position - transform.position;
-        }
-        
-        m_rotationController.SetDirect(m_moveDirect);
+        SettingPosition();   //場所の移動
+        SettingMoveDirect(); //移動方向の設定
 
+        //欲しいコンポーネントのOn
         m_rotationController.enabled = true;
         m_velocityManager.enabled = true;
+        m_throngManager.enabled = true;
 
-        var time = m_audioPlayRange.RandomValue;
-        m_waitTimer.AddWaitTimer(GetType(), time, () => m_audioManager?.PlayOneShot());
-        //m_audioManager?.PlayOneShot();
+        PlayShoutSE(); //声の再生
     }
 
     void ChangeEnableBehabiours(bool enable = false)
@@ -85,6 +90,42 @@ public class ClearManager_Zombie : MonoBehaviour
         {
             behaviour.enabled = enable;
         }
+    }
+
+    private void CrossFadeAnimation()
+    {
+        m_animatorManager.CrossFadeIdleAnimation();
+        m_animatorManager.CrossFadeIdleAnimation(m_animatorManager.UpperLayerIndex);
+        m_animatorManager.CrossFadeIdleAnimation(m_animatorManager.AllLayerIndex);
+    }
+
+    private void SettingPosition()
+    {
+        //扉に平行に合わせて、ランダムに生成。
+        var movePosition = m_barricade.transform.position + m_barricade.transform.up;
+        movePosition += m_barricade.transform.right * m_positionRandomRange.RandomValue;
+
+        transform.position = movePosition;
+    }
+
+    private void SettingMoveDirect()
+    {
+        m_moveDirect = transform.forward;
+        if (m_barricade)
+        {  //バリケードの方向に向かって走り出す。
+            //m_moveDirect = m_barricade.transform.position - transform.position;
+            m_moveDirect = -m_barricade.transform.up;
+            m_moveDirect = Quaternion.Euler(0.0f, m_directRandomRange.RandomValue, 0.0f) * m_moveDirect;
+        }
+
+        transform.forward = m_moveDirect;
+        m_rotationController.SetDirect(m_moveDirect);
+    }
+
+    private void PlayShoutSE()
+    {
+        var time = m_audioPlayRange.RandomValue;
+        m_waitTimer.AddWaitTimer(GetType(), time, () => m_audioManager?.PlayOneShot());
     }
 
     private void OnCollisionEnter(Collision collision)
