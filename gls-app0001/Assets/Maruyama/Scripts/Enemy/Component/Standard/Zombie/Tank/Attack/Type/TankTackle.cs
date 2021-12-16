c529eacd1adb1c509b1470596f15805c5f7c0472
Maru <zeal20300058@fsgcl.jp>
2021-12-16 17:25:38 +0900

    2021WIZ-569
    TankTackleで突進中に破壊オブジェクトでは止まらないようにした。

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/Standard/Zombie/Tank/Attack/Type/TankTackle.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Component/Standard/Zombie/Tank/Attack/Type/TankTackle.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UniRx;
 
 using MaruUtility;
+using AttributeObject;
 
 public class TankTackle : AttackNodeBase
 {
@@ -17,43 +18,45 @@ public class TankTackle : AttackNodeBase
     }
 
     [SerializeField]
-    float m_tackleSpeed = 15.0f;
+    private float m_tackleSpeed = 15.0f;
 
     [SerializeField]
-    float m_tackleLastRange = 2.5f;
+    private float m_tackleLastRange = 2.5f;
 
     [SerializeField]
-    float m_eyeDeg = 40.0f;
-    float m_eyeRad = 0.0f;
+    private float m_eyeDeg = 40.0f;
+    private float m_eyeRad = 0.0f;
 
     [SerializeField]
-    float m_turningPower = 3.0f;
+    private float m_turningPower = 3.0f;
 
     /// <summary>
     /// //ターゲットとのフォワードの差がこの数字よりより小さければ、予測タックルにする。
     /// </summary>
     [SerializeField]
-    float m_subPursuitTargetForward = 0.3f;
+    private float m_subPursuitTargetForward = 0.3f;
 
     [Header("タックル終了後の待機時間"),SerializeField]
-    float m_waitTime = 1.0f; //タックル終了後の待機時間
+    private float m_waitTime = 1.0f; //タックル終了後の待機時間
 
     /// <summary>
     /// Rayの障害物するLayerの配列
     /// </summary>
     [SerializeField]
-    string[] m_rayObstacleLayerStrings = new string[] { "L_Obstacle" };
+    private string[] m_rayObstacleLayerStrings = new string[] { "L_Obstacle" };
 
-    ReactiveProperty<State> m_state = new ReactiveProperty<State>(State.None);
+    private bool m_isAttack = false;
 
-    TargetManager m_targetManager;
-    EnemyVelocityMgr m_velocityManager;
-    Stator_ZombieTank m_stator;
-    EyeSearchRange m_eye;
-    AnimatorManager_ZombieTank m_animatorManager;
-    WaitTimer m_waitTimer;
+    private ReactiveProperty<State> m_state = new ReactiveProperty<State>(State.None);
 
-    TaskList<State> m_taskList = new TaskList<State>();
+    private TargetManager m_targetManager;
+    private EnemyVelocityMgr m_velocityManager;
+    private Stator_ZombieTank m_stator;
+    private EyeSearchRange m_eye;
+    private AnimatorManager_ZombieTank m_animatorManager;
+    private WaitTimer m_waitTimer;
+
+    private TaskList<State> m_taskList = new TaskList<State>();
 
     private void Awake()
     {
@@ -74,7 +77,7 @@ public class TankTackle : AttackNodeBase
         TackleMove();
     }
 
-    Vector3 CalcuToTargetVec()
+    private Vector3 CalcuToTargetVec()
     {
         var target = m_targetManager.GetNowTarget();
         if(target == null) {
@@ -85,7 +88,7 @@ public class TankTackle : AttackNodeBase
         return toVec;
     }
 
-    void TackleMove()
+    private void TackleMove()
     {
         if (m_state.Value != State.Tackle) {  //Tackleでなかったら処理をしない
             return;
@@ -94,8 +97,6 @@ public class TankTackle : AttackNodeBase
         Vector3 force = CalcuTackleForce();
 
         m_velocityManager.AddForce(force);
-        //Debug.Log(m_velocityManager.velocity.magnitude);
-        //m_animatorManager.TackleSpeed = m_velocityManager.velocity.magnitude / m_tackleSpeed;
 
         if (IsTackleEnd()) //ターゲットに限りなく近づいたら
         {
@@ -103,7 +104,7 @@ public class TankTackle : AttackNodeBase
         }
     }
 
-    Vector3 CalcuTackleForce()
+    private Vector3 CalcuTackleForce()
     {
         var target = m_targetManager.GetNowTarget();
         if(target == null) { 
@@ -129,7 +130,7 @@ public class TankTackle : AttackNodeBase
         }
     }
 
-    void Rotation()
+    private void Rotation()
     {
         var forward = m_state.Value switch {
             State.Charge => CalcuToTargetVec().normalized,
@@ -183,7 +184,7 @@ public class TankTackle : AttackNodeBase
     /// </summary>
     /// <param name="toTargetVec">ターゲットの方向</param>
     /// <returns>正面ならtrue</returns>
-    bool IsFront(Vector3 toTargetVec)
+    private bool IsFront(Vector3 toTargetVec)
     {
         return Vector3.Dot(toTargetVec, transform.forward) > 0 ? true : false;
     }
@@ -193,7 +194,7 @@ public class TankTackle : AttackNodeBase
     /// </summary>
     /// <param name="relativeHeading">自分と相手のフォワードのdot</param>
     /// <returns>差が開いていなかったらtrue</returns>
-    bool IsMinSubForward(float relativeHeading, float sub)
+    private bool IsMinSubForward(float relativeHeading, float sub)
     {
         return Mathf.Abs(relativeHeading) > sub ? true : false;
     }
@@ -202,7 +203,7 @@ public class TankTackle : AttackNodeBase
     /// タックルの終了条件
     /// </summary>
     /// <returns></returns>
-    bool IsTackleEnd()
+    private bool IsTackleEnd()
     {
         var speedRate = m_velocityManager.velocity.magnitude / m_tackleSpeed;
         var range = m_tackleLastRange * speedRate;
@@ -210,7 +211,7 @@ public class TankTackle : AttackNodeBase
             return true;
         }
 
-        if (!IsTackleEyeRad()) {
+        if (!IsTackleEyeRange()) {  //視界から外れたらタックル終了
             return true;
         }
 
@@ -221,17 +222,24 @@ public class TankTackle : AttackNodeBase
     /// タックルの追従する角度
     /// </summary>
     /// <returns></returns>
-    public bool IsTackleEyeRad()
+    public bool IsTackleEyeRange()
     {
         var target = m_targetManager.GetNowTarget();
         if(target == null) {
             return false;
         }
 
-        var eyeParam = m_eye.GetParam();
-        eyeParam.rad = m_eyeRad;
-        if (m_eye.IsInEyeRange(m_targetManager.GetNowTarget().gameObject, eyeParam))
-        {
+        //const float eyeRange = 50.0f;  //タックル中の視界の広さ
+        //var eyeParam = m_eye.GetParam();
+        //eyeParam.rad = m_eyeRad;
+        //eyeParam.range = eyeRange;
+        //if (m_eye.IsInEyeRange(m_targetManager.GetNowTarget().gameObject, eyeParam))
+        //{
+        //    return true;
+        //}
+
+        var toTargetVec = (Vector3)m_targetManager.GetToNowTargetVector();
+        if (UtilityMath.IsFront(transform.forward , toTargetVec)){  //正面にいるなら
             return true;
         }
 
@@ -253,37 +261,60 @@ public class TankTackle : AttackNodeBase
     }
 
     /// <summary>
-    /// 壁に突っかかりを回避
+    /// 壁の突っかかりを回避
     /// </summary>
     /// <param name="collision">コリジョン</param>
-    void TakcleWallAvoid(Collision collision)
+    private void TackleWallAvoid(Collision collision)
     {
         foreach (var layerString in m_rayObstacleLayerStrings)
         {
             int obstacleLayer = LayerMask.NameToLayer(layerString);
             if (collision.gameObject.layer == obstacleLayer) {  //障害物と当たったら
-                //TackleLastStart();  //タックルを終了
+                TackleLastStart();  //タックルを終了
             }
         }
     }
 
+    //タックル中に壁に突っかかっているかどうか
+    private bool IsTackleWallAvoid(Collision collision)
+    {
+        if(m_state.Value != State.Tackle) { //ステートがTackleでない場合は処理をしない。
+            return false;
+        }
+
+        //対象が破壊できる壁ならfalse
+        var typeDamage = collision.gameObject.GetComponent<TypeDamageDestroy>();
+        if (typeDamage)
+        {
+            foreach (DamageType type in typeDamage.GetTakeDamageTypes())
+            {
+                if (type == DamageType.ObjectBreak)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        if(m_state.Value == State.Tackle) {  //タックル状態で
-            TakcleWallAvoid(collision);  //壁の突っかかり回避
+        if(IsTackleWallAvoid(collision)) {  //タックル状態で
+            TackleWallAvoid(collision);  //壁の突っかかり回避
         }
 
         //攻撃状態なら
         if (m_state.Value == State.Tackle || m_state.Value == State.TackleLast) {
-            var damage = collision.gameObject.GetComponent<AttributeObject.TakeDamageObject>();
+            var damage = collision.gameObject.GetComponent<TakeDamageObject>();
             damage?.TakeDamage(GetBaseParam().damageData);
         }
     }
 
     private void OnCollisionStay(Collision collision)
     {
-        if (m_state.Value == State.Tackle) {  //タックル状態で
-            TakcleWallAvoid(collision);  //壁の突っかかり回避
+        if (IsTackleWallAvoid(collision)) {  //タックル状態で
+            TackleWallAvoid(collision);  //壁の突っかかり回避
         }
     }
 
