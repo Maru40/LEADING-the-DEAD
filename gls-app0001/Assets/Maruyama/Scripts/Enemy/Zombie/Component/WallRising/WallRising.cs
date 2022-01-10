a20932d13728762dab224dd299f23f8e1ec00fbf
Maru <zeal20300058@fsgcl.jp>
2022-01-11 01:04:06 +0900

    2021WIZ-632
    WallRisingの生成

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Zombie/Component/WallRising/WallRising.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 壁のぼり
+/// </summary>
+public class WallRising : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
