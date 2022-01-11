2871a75b7540de9a82dbaedde08f4186b39521f1
Maru <zeal20300058@fsgcl.jp>
2022-01-11 12:06:23 +0900

    2021WIZ-656
    UtilitySoudParameotにIntervalTimeを実装

--- gls-app0001/Assets/Maruyama/Scripts/Utility/MaruUtility/UtilitySound.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Utility/MaruUtility/UtilitySound.cs
@@ -7,15 +7,45 @@ namespace MaruUtility
     namespace Sound
     {
         [System.Serializable]
-        public struct AudioClipParametor
+        public class AudioClipParametor
         {
             public AudioClip clip;
             public float volume;
 
+            public float intervalTime;
+
+            private GameTimer timer = new GameTimer();
+
+            public AudioClipParametor()
+                :this(null, 0.0f, 0.0f)
+            {}
+
             public AudioClipParametor(AudioClip clip, float volume)
+                :this(clip, volume, 0.0f)
+            { }
+
+            public AudioClipParametor(AudioClip clip, float volume, float intervalTime)
             {
                 this.clip = clip;
                 this.volume = volume;
+                this.intervalTime = intervalTime;
+
+                timer = new GameTimer();
+                timer.ResetTimer(0.0f);
+            }
+
+            public void TimerUpdate()
+            {
+                timer.UpdateTimer();
+            }
+
+            public void SEPlayOneShot()
+            {
+                if (timer.IsTimeUp)
+                {
+                    Manager.GameAudioManager.Instance.SEPlayOneShot(clip, volume);
+                    timer.ResetTimer(intervalTime);
+                }
             }
         }
     }
