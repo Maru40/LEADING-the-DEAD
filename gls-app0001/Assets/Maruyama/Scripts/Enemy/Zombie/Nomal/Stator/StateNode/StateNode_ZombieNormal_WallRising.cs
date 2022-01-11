63991cdd11ac2130325ea666d0c56df69c9053d0
Maru <zeal20300058@fsgcl.jp>
2022-01-11 09:13:14 +0900

    2021WIZ-632
    StateNodeWallRisingの実装

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Zombie/Nomal/Stator/StateNode/StateNode_ZombieNormal_WallRising.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateNode_ZombieNormal_WallRising : EnemyStateNodeBase<EnemyBase>
+{
+    public StateNode_ZombieNormal_WallRising(EnemyBase owner)
+        :base(owner)
+    {
+
+    }
+
+    protected override void ReserveChangeComponents()
+    {
+
+    }
+
+    public override void OnStart()
+    {
+        base.OnStart();
+    }
+
+    public override void OnUpdate()
+    {
+
+    }
+
+    public override void OnExit()
+    {
+        base.OnExit();
+    }
+}
