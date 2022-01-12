7f6049568c8eb3f3d7358d4a6bd9b7a793085a1f
Maru <zeal20300058@fsgcl.jp>
2022-01-12 13:22:01 +0900

    2021WIZ-665
    SoundObjectManagerの生成

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Zombie/Component/SoundObjectManager/SoundObjectManager.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events;
+
+/// <summary>
+/// 音を出すオブジェクトの管理
+/// </summary>
+[RequireComponent(typeof(SphereCollider))]
+[RequireComponent(typeof(AudioManager))]
+public class SoundObjectManager : MonoBehaviour
+{
+    [System.Serializable]
+    private struct Parametor
+    {
+        [Header("音の範囲")]
+        public float range;
+        [Header("音の時間")]
+        public float time;
+    }
+
+    [SerializeField]
+    private Parametor m_param = new Parametor();
+
+    [SerializeField]
+    private FoundObject m_foundObject = null;
+
+    private SphereCollider m_collider;
+    private AudioManager m_audioManager;
+
+    private GameTimer m_timer = new GameTimer();
+
+    [SerializeField]
+    private UnityEvent m_event = new UnityEvent();
+
+    private void Awake()
+    {
+        m_collider = GetComponent<SphereCollider>();
+        m_collider.isTrigger = true;
+        //m_collider.enabled = false;
+        m_audioManager = GetComponent<AudioManager>();
+
+        NullCheck();
+    }
+
+    private void Start()
+    {
+        m_collider.radius = m_param.range;
+    }
+
+    private void Update()
+    {
+        m_timer.UpdateTimer();
+    }
+
+    /// <summary>
+    /// 音の再生
+    /// </summary>
+    public void PlayOneShot()
+    {
+        m_audioManager.PlayOneShot();
+
+        m_collider.enabled = true;  //音検知用のコライダーをOn
+        m_timer.ResetTimer(m_param.time, () => m_collider.enabled = false); //指定時間後に音検知用のコライダーをoff
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(enabled == false) {
+            return;
+        }
+
+        var ear = other.GetComponent<EarBase>();
+        if(ear)
+        {
+            ear.Listen(m_foundObject);
+        }
+    }
+
+    private void NullCheck()
+    {
+        if (m_foundObject == null)
+        {
+            m_foundObject = GetComponentInParent<FoundObject>();
+        }
+    }
+
+    public void ChangeState(ZombieNormalState ss)
+    {
+
+    }
+
+    //アクセッサ----------------------------------------------------------------------------------
+
+    public float Range
+    {
+        get => m_param.range;
+        set
+        {
+            m_param.range = value;
+            m_collider.radius = value;
+        }
+    }
+
+}
