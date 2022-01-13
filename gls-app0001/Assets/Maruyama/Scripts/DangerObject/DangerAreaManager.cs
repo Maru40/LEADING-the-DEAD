168443ded97e9091feba5e30937645ecaf17b35c
Maru <zeal20300058@fsgcl.jp>
2022-01-13 16:14:25 +0900

    2021WIZ-688
    DangerAreaManagerの生成

---
+++ gls-app0001/Assets/Maruyama/Scripts/DangerObject/DangerAreaManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DangerAreaManager : MonoBehaviour
+{
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(enabled == false) {
+            return;
+        }
+
+        var evasion = other.GetComponent<I_DangerEvasion>();
+        evasion?.Evasion(gameObject);
+    }
+}
