206215de7f690220fe38dbf47ec0e606a95e8c85
Maru <zeal20300058@fsgcl.jp>
2022-01-14 18:14:33 +0900

    2021WIZ-694
    BlackBoard＿ZombieNormalの生成

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/BlackBoard/BlackBoard_ZombieNormal.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Parametor = BlackBoard_ZombieNormal.Parametor;
+
+public class BlackBoard_ZombieNormal : MonoBehaviour, I_BlackBoard<Parametor>
+{
+    [System.Serializable]
+    public struct Parametor
+    {
+        public TargetManager targetManager;
+        public StateNode_ZombieNormal_Attack.AttackType attackType;
+    }
+
+    [SerializeField]
+    private Parametor m_param;
+
+    private void Start()
+    {
+        if(m_param.targetManager == null)
+        {
+            m_param.targetManager = GetComponent<TargetManager>();
+        }
+    }
+
+    public Parametor Struct
+    {
+        get => m_param;
+        set => m_param = value;
+    }
+
+    public Parametor GetStruct()
+    {
+        return m_param;
+    }
+}
