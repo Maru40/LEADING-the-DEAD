dcd9809b703848e97be3111290ffdf816dbaf6a8
Maru <zeal20300058@fsgcl.jp>
2022-01-14 18:13:43 +0900

    2021WIZ-694
    I_BrackBoardの生成

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/BlackBoard/I_BlackBoard.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface I_BlackBoard<StructType>
+    where StructType : struct
+{
+    public StructType Struct { get; set; }
+
+    public StructType GetStruct();
+}
