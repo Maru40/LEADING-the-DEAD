a3f47edde49e01c8e5bc573e88d2613942bc8bd0
Maru <zeal20300058@fsgcl.jp>
2022-01-17 14:27:59 +0900

    2021WIZ-694
    StateNode＿ZombieNormalでステートをタスクリストで管理するようにした。

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/Zombie/Nomal/Stator/StateNode/StateNode_ZombieNormal_Attack.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Zombie/Nomal/Stator/StateNode/StateNode_ZombieNormal_Attack.cs
@@ -4,38 +4,57 @@ using UnityEngine;
 
 public class StateNode_ZombieNormal_Attack : EnState_AttackBase
 {
-    public enum AttackType
+    public enum StateType
     {
+        None,
         Normal,
         Dash,
         WallAttack,
+        Max
     }
 
-    private enum TaskEnum
+    //public class TransitionMember
+    //{
+    //    public MyTrigger normalTrigger;
+    //    public MyTrigger dashTrigger;
+    //    public MyTrigger wallDashTrigger;
+    //    public MyTrigger wallPutTrigger;
+    //}
+
+    public struct BlackBoardParametor
     {
-        Wait, //待機
-        Preliminary, //予備動作
+        public StateType startType;
     }
 
-    private TaskList<TaskEnum> m_taskList = new TaskList<TaskEnum>();
+    [System.Serializable]
+    public struct Parametor
+    {
+        [Header("通常攻撃")]
+        public AttackNode_Normal.Parametor normalParam;
+        [Header("ダッシュ攻撃")]
+        public AttackNode_Dash.Parametor dashParam;
+        [Header("壁攻撃")]
+        public AttackNode_WallDash.Parametor wallAttackParam;
+    }
+
+    private Parametor m_param =  new Parametor();
 
     private TargetManager m_targetManager;
     private Stator_ZombieNormal m_stator;
     private BlackBoard_ZombieNormal m_blackBoard;
 
-    public StateNode_ZombieNormal_Attack(EnemyBase owner)
+    private TaskList<StateType> m_taskList = new TaskList<StateType>();
+
+    public StateNode_ZombieNormal_Attack(EnemyBase owner, Parametor parametor)
         : base(owner)
     {
+        m_param = parametor;
+
         m_targetManager = owner.GetComponent<TargetManager>();
         m_stator = owner.GetComponent<Stator_ZombieNormal>();
         m_blackBoard = owner.GetComponent<BlackBoard_ZombieNormal>();
 
         DefineTask();
-    }
-
-    protected override void PlayStartAnimation()
-    {
-        
     }
 
     protected override void ReserveChangeComponents()
@@ -50,6 +69,7 @@ public class StateNode_ZombieNormal_Attack : EnState_AttackBase
     {
         base.OnStart();
 
+        //SelectState();
         SelectTask();
     }
 
@@ -58,12 +78,13 @@ public class StateNode_ZombieNormal_Attack : EnState_AttackBase
         base.OnUpdate();
 
         m_taskList.UpdateTask();
-        if(m_taskList.IsEnd)
+
+        if (m_taskList.IsEnd)
         {
-            m_stator.GetTransitionMember().rondomPlowlingTrigger.Fire();
-            return;
+            m_stator.GetTransitionMember().chaseTrigger.Fire();
         }
 
+        //ターゲットが存在しない
         if (!m_targetManager.HasTarget())
         {
             m_stator.GetTransitionMember().rondomPlowlingTrigger.Fire();
@@ -75,20 +96,28 @@ public class StateNode_ZombieNormal_Attack : EnState_AttackBase
         base.OnExit();
 
         m_taskList.AbsoluteReset();
+        m_blackBoard.Struct.attackParam.startType = StateType.None;
     }
 
     private void DefineTask()
     {
+        var enemy = GetOwner();
 
+        m_taskList.DefineTask(StateType.Normal, new AttackNode_Normal(enemy, m_param.normalParam));
+
+        m_taskList.DefineTask(StateType.Dash, new AttackNode_Dash(enemy, m_param.dashParam));
+
+        m_taskList.DefineTask(StateType.WallAttack, new AttackNode_WallDash(enemy, m_param.wallAttackParam));
     }
 
     private void SelectTask()
     {
-        TaskEnum[] tasks = m_blackBoard.Struct.attackType switch {
-            AttackType.Normal => new TaskEnum[]{ TaskEnum.Wait },
-            AttackType.Dash => new TaskEnum[] { },
-            AttackType.WallAttack => new TaskEnum[] { },
-            _ => null
+        StateType[] tasks = m_blackBoard.GetStruct().attackParam.startType switch
+        {
+            StateType.Normal => new StateType[] { StateType.Normal },
+            StateType.Dash => new StateType[] { StateType.Dash },
+            StateType.WallAttack => new StateType[] { StateType.WallAttack, },
+            _ => new StateType[] { }
         };
 
         foreach(var task in tasks)
