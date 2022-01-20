a6b64c6f65ea753d40cf579a9e3d960523d9b29a
Maru <zeal20300058@fsgcl.jp>
2022-01-20 16:21:26 +0900

    2021WIZ-703
    StateNode_ZombieChild_Plowlingの生成

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Zombie/Child/Stator/StateNode/StateNode_ZombieChild_Plowling.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateNode_ZombieChild_Plowling : EnemyStateNodeBase<EnemyBase>
+{
+    private FoundObject m_eyeTarget = null;
+
+    private TargetManager m_targetManager;
+    private EyeSearchRange m_eye;
+
+    public StateNode_ZombieChild_Plowling(EnemyBase owner)
+        :base(owner)
+    {
+        m_targetManager = owner.GetComponent<TargetManager>();
+        m_eye = owner.GetComponent<EyeSearchRange>();
+    }
+
+    protected override void ReserveChangeComponents()
+    {
+
+    }
+
+    public override void OnStart()
+    {
+        base.OnStart();
+    }
+
+    public override void OnUpdate()
+    {
+        TargetCheck();
+    }
+
+    public override void OnExit()
+    {
+        base.OnExit();
+    }
+
+    private void TargetCheck()
+    {
+        //ターゲットが視界に入ったら切り替える。
+        if (m_eyeTarget == null) {
+            SearchEyeTarget();
+        }
+
+        if (m_eye.IsInEyeRange(m_eyeTarget.gameObject)) { //視界の中にいたら
+            m_targetManager.SetNowTarget(GetType(), m_eyeTarget);  //ターゲットの変更
+        }
+    }
+
+    private void SearchEyeTarget()
+    {
+        var target = GameObject.Find("Player");
+        var foundObject = target.GetComponent<FoundObject>();
+
+        m_eyeTarget = foundObject;
+    }
+}
