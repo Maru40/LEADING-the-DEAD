a5340bbcfa1a683d75adecf5cdaa0204078847e0
Maru <zeal20300058@fsgcl.jp>
2022-01-20 16:21:44 +0900

    2021WIZ-703
    StatusManager＿ZombieChildを生成

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Zombie/Child/Status/StatusManager_ZombieChild.cs
@@ -0,0 +1,18 @@
+﻿using AttributeObject;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusManager_ZombieChild : StatusManagerBase
+{
+
+    private void Update()
+    {
+        
+    }
+
+    public override void Damage(DamageData data)
+    {
+
+    }
+}
