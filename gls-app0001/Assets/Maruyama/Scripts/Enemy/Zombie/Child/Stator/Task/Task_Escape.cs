a87d3d0406361370f646a4df8bbc2fe63fd89f27
Maru <zeal20300058@fsgcl.jp>
2022-01-21 15:45:41 +0900

    2021WIZ-712
    Task_Escapeを更新

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/Zombie/Child/Stator/Task/Task_Escape.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Zombie/Child/Stator/Task/Task_Escape.cs
@@ -2,17 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Task_Escape : MonoBehaviour
+using MaruUtility;
+
+public class Task_Escape : TaskNodeBase<EnemyBase>
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public struct Parametor
     {
-        
+        public float maxSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private Parametor m_param = new Parametor();
+
+    private TargetManager m_targetManager;
+    private EnemyVelocityManager m_velocityManager;
+    private EnemyRotationCtrl m_rotationController;
+
+    public Task_Escape(EnemyBase owner, Parametor parametor)
+        :base(owner)
     {
-        
+        m_param = parametor;
+
+        m_targetManager = owner.GetComponent<TargetManager>();
+        m_velocityManager = owner.GetComponent<EnemyVelocityManager>();
+        m_rotationController = owner.GetComponent<EnemyRotationCtrl>();
+    }
+
+    public override void OnEnter()
+    {
+        base.OnEnter();
+    }
+
+    public override bool OnUpdate()
+    {
+        Move();
+        Rotation();
+
+        return IsEnd();
+    }
+
+    public override void OnExit()
+    {
+        base.OnExit();
+    }
+
+    private void Move()
+    {
+        if (!m_targetManager.HasTarget()) {
+            return;
+        }
+
+        var toTargetVec = (Vector3)m_targetManager.GetToNowTargetVector();
+
+        var velocity = m_velocityManager.velocity;
+        var force = CalcuVelocity.CalucSeekVec(velocity ,toTargetVec, m_param.maxSpeed);
+
+        m_velocityManager.AddForce(force);
+    }
+
+    private void Rotation()
+    {
+        m_rotationController.SetDirect(m_velocityManager.velocity);
+    }
+
+    private bool IsEnd()
+    {
+        if (!m_targetManager.HasTarget()) {
+            return true;
+        }
+
+        return false;
     }
 }
