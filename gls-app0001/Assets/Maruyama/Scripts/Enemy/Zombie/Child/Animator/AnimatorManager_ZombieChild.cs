09b73f5e0ddd9e0a61fe4e8d26467750a078759c
Maru <zeal20300058@fsgcl.jp>
2022-01-21 16:51:19 +0900

    2021WIZ-712
    AnimatorManager_ZombieChildで逃げる子供ゾンビのAnimator適応

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Zombie/Child/Animator/AnimatorManager_ZombieChild.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorManager_ZombieChild : MonoBehaviour
+{
+    [SerializeField]
+    private Animator m_animator;
+
+    private Rigidbody m_rigid;
+
+    private void Awake()
+    {
+        m_rigid = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (m_animator == null)
+        {
+            m_animator = GetComponentInChildren<Animator>();
+        }
+
+        //歩き同期
+        moveSpeed = m_rigid.velocity.magnitude;
+    }
+
+    public float moveSpeed
+    {
+        set { m_animator.SetFloat("moveSpeed", value); }
+        get { return m_animator.GetFloat("moveSpeed"); }
+    }
+}
