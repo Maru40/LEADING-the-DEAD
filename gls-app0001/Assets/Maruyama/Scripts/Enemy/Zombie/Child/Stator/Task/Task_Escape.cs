5b247f9a58d3a5743279cd204dfe357841cb5f41
Maru <zeal20300058@fsgcl.jp>
2022-01-21 16:09:51 +0900

    2021WIZ-712
    Task＿Escapeの実装

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/Zombie/Child/Stator/Task/Task_Escape.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Zombie/Child/Stator/Task/Task_Escape.cs
@@ -10,6 +10,8 @@ public class Task_Escape : TaskNodeBase<EnemyBase>
     public struct Parametor
     {
         public float maxSpeed;
+        [Header("壁突っかかり回避時間")]
+        public float wallEvasionTime;
     }
 
     private Parametor m_param = new Parametor();
@@ -17,6 +19,10 @@ public class Task_Escape : TaskNodeBase<EnemyBase>
     private TargetManager m_targetManager;
     private EnemyVelocityManager m_velocityManager;
     private EnemyRotationCtrl m_rotationController;
+    private CollisionAction m_collisionAction;
+
+    private GameTimer m_timer = new GameTimer();
+    private Vector3 m_reflectionVec = new Vector3();
 
     public Task_Escape(EnemyBase owner, Parametor parametor)
         :base(owner)
@@ -26,17 +32,30 @@ public class Task_Escape : TaskNodeBase<EnemyBase>
         m_targetManager = owner.GetComponent<TargetManager>();
         m_velocityManager = owner.GetComponent<EnemyVelocityManager>();
         m_rotationController = owner.GetComponent<EnemyRotationCtrl>();
+
+        m_collisionAction = owner.GetComponent<CollisionAction>();
+        m_collisionAction.AddEnterAction(CollisionHit);
     }
 
     public override void OnEnter()
     {
         base.OnEnter();
+
+        m_timer.ResetTimer(0.0f);
     }
 
     public override bool OnUpdate()
     {
-        Move();
-        Rotation();
+        if (m_timer.IsTimeUp)
+        {
+            Move();
+            Rotation();
+        }
+        else
+        {
+            m_timer.UpdateTimer();
+            ReflectionMove();
+        }
 
         return IsEnd();
     }
@@ -55,7 +74,15 @@ public class Task_Escape : TaskNodeBase<EnemyBase>
         var toTargetVec = (Vector3)m_targetManager.GetToNowTargetVector();
 
         var velocity = m_velocityManager.velocity;
-        var force = CalcuVelocity.CalucSeekVec(velocity ,toTargetVec, m_param.maxSpeed);
+        var force = CalcuVelocity.CalucSeekVec(velocity ,-toTargetVec, m_param.maxSpeed);
+
+        m_velocityManager.AddForce(force);
+    }
+
+    private void ReflectionMove()
+    {
+        var velocity = m_velocityManager.velocity;
+        var force = CalcuVelocity.CalucSeekVec(velocity, m_reflectionVec, m_param.maxSpeed);
 
         m_velocityManager.AddForce(force);
     }
@@ -63,6 +90,15 @@ public class Task_Escape : TaskNodeBase<EnemyBase>
     private void Rotation()
     {
         m_rotationController.SetDirect(m_velocityManager.velocity);
+    }
+
+    private void CollisionHit(Collision other)
+    {
+        if (m_timer.IsTimeUp)
+        {
+            m_reflectionVec = CalcuVelocity.Reflection(m_velocityManager.velocity, other);
+            m_timer.ResetTimer(m_param.wallEvasionTime);
+        }
     }
 
     private bool IsEnd()
