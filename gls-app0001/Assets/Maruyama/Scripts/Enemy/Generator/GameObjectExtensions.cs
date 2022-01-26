790c676fb57cb2acd39a87202d4c1af10416e585
Maru <zeal20300058@fsgcl.jp>
2022-01-26 13:16:20 +0900

    2021WIZ-722
    GameObjectExtensionsの実装

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Generator/GameObjectExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MaruUtility
+{
+    public static class GameObjectExtensions
+    {
+        /// <summary>
+        /// 指定されたインターフェイスを実装したコンポーネントを持つオブジェクトを検索する。
+        /// </summary>
+        public static List<T> FindObjectsOfInterface<T>() where T : class
+        {
+            var list = new List<T>();
+
+            foreach (var component in GameObject.FindObjectsOfType<Component>())
+            {
+                var t = component as T;
+                if (t != null)
+                {
+                    list.Add(t);
+                }
+            }
+
+            return list;
+        }
+    }
+}
