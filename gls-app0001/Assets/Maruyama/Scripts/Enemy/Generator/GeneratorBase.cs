00b430f2506cb3ce33ad212ac1b526464634982a
Maru <zeal20300058@fsgcl.jp>
2022-01-26 11:28:32 +0900

    2021WIZ-722
    GeneratorBaseを基底クラスに変更

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/Generator/GeneratorBase.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Generator/GeneratorBase.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GeneratorBase : MonoBehaviour
+public abstract class GeneratorBase : MonoBehaviour
 {
     private uint m_deathCount = 0;
 
