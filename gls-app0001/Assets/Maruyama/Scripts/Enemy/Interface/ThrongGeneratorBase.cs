8591daab60f0bc2405550f5ed24d384d1392ef2f
Maru <zeal20300058@fsgcl.jp>
2022-01-26 11:17:02 +0900

    2021WIZ-722
    ThrongGeneratorBaseで一部の基底関数を追加

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Interface/ThrongGeneratorBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ThrongGeneratorBase : GeneratorBase
+{
+    //生成したゾンビを持つ
+    public abstract List<ThrongData> ThrongDatas { get; }
+    public abstract int NumCreate { get; }
+    public abstract int GetNumAlive();
+    public abstract bool IsInCameraCreate { get; set; }
+    public abstract void RepawnPositoinAll();
+}
