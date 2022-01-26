4beb540efeb01ecb984269b22c44a2825b1a4ece
Maru <zeal20300058@fsgcl.jp>
2022-01-26 14:15:05 +0900

    2021WIZ-722
    ThrongGeneratorBaseでよく使う計算を親クラスに置いた。

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/Interface/ThrongGeneratorBase.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Interface/ThrongGeneratorBase.cs
@@ -11,4 +11,32 @@ public abstract class ThrongGeneratorBase : GeneratorBase
     public abstract bool IsInCameraCreate { get; set; }
     public abstract void RepawnPositoinAll();
     public abstract Vector3 CalcuRandomPosition();
+
+    protected ThrongData CalcuThrongData(GameObject obj)
+    {
+        var newData = new ThrongData(obj.GetComponent<EnemyVelocityManager>(),
+            obj.GetComponent<TargetManager>(),
+            obj.GetComponent<ThrongManager>(),
+            obj.GetComponent<RandomPlowlingMove>(),
+            obj.GetComponent<DropObjecptManager>(),
+            obj.GetComponent<ClearManager_Zombie>(),
+            obj.GetComponent<EnemyRespawnManager>()
+        );
+
+        return newData;
+    }
+
+    protected int CalcuNumAlive(List<ThrongData> datas)
+    {
+        int count = 0;
+        foreach (var data in datas)
+        {
+            if (data.gameObject.activeSelf)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
