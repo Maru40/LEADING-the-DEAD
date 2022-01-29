3a2aee5119a761e4b0e9f038123bdfdba7f4a92b
Maru <zeal20300058@fsgcl.jp>
2022-01-30 07:09:50 +0900

    2021WIZ-718
    Task＿FireDyningで死亡時のパーティクルを表示できるようにした。

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/Zombie/Child/Stator/Task/Dyning/Task_FireDyning.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Zombie/Child/Stator/Task/Dyning/Task_FireDyning.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using ParticleData = ParticleManager.ParticleData;
+
 public class Task_FireDining : TaskNodeBase<EnemyBase>
 {
     [System.Serializable]
     public struct Parametor
     {
+        public ParticleData particleData;
         public float time;
+
+        public List<AudioManager_Ex.Parametor> audioParams;
     }
 
     private Parametor m_param = new Parametor();
     private GameTimer m_timer = new GameTimer();
 
     private EnemyVelocityManager m_velocityManager;
+    private AudioManager_Ex m_audioManager;
 
     public Task_FireDining(EnemyBase owner, Parametor parametor)
         : base(owner)
@@ -21,13 +27,16 @@ public class Task_FireDining : TaskNodeBase<EnemyBase>
         m_param = parametor;
 
         m_velocityManager = owner.GetComponent<EnemyVelocityManager>();
+        m_audioManager = owner.GetComponent<AudioManager_Ex>();
     }
 
     public override void OnEnter()
     {
         base.OnEnter();
 
+        CreateParticle();
         m_velocityManager.StartDeseleration();
+        m_audioManager.PlayRandomClipOneShot(m_param.audioParams);
         m_timer.ResetTimer(m_param.time);
     }
 
@@ -43,5 +52,11 @@ public class Task_FireDining : TaskNodeBase<EnemyBase>
         base.OnExit();
 
         m_velocityManager.SetIsDeseleration(false);  //減速終了
+    }
+
+    private void CreateParticle()
+    {
+        //var particle = ParticleManager.Instance.Play(m_param.particleData.id, m_param.particleData.CreatePosition);
+        //particle.transform.parent = GetOwner().transform;
     }
 }
