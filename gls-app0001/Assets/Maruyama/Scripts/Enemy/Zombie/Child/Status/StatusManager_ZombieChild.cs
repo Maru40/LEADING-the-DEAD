9bef9b4d76ee8eadb977874c794724b5aab70d5d
Maru <zeal20300058@fsgcl.jp>
2022-01-30 04:13:01 +0900

    2021WIZ-718
    StatusManager＿ZombieChildでDamage関数の実装

--- gls-app0001/Assets/Maruyama/Scripts/Enemy/Zombie/Child/Status/StatusManager_ZombieChild.cs
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Zombie/Child/Status/StatusManager_ZombieChild.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 
 public class StatusManager_ZombieChild : StatusManagerBase
 {
+    [Header("ダメージを受けるタイプ"), SerializeField]
+    public List<DamageType> m_damageTypes = new List<DamageType>() { DamageType.Fire };
 
-    private void Update()
+    private DamageManager_ZombieChild m_damageManager;
+
+    private void Awake()
     {
-        
+        m_damageManager = new DamageManager_ZombieChild(gameObject);
     }
 
     public override void Damage(DamageData data)
     {
-
+        if(data.IsType(m_damageTypes.ToArray()))
+        {
+            m_damageManager.Damaged(data);
+        }
     }
 }
