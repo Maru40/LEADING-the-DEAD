6b4fc6cd467abd30101ab3ad0a062bbdabcfd344
Maru <zeal20300058@fsgcl.jp>
2022-01-30 04:07:03 +0900

    2021WIZ-718
    StateNode＿ZombieChild＿Deathの生成

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Zombie/Child/Stator/StateNode/StateNode_ZombieChild_Death.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateNode_ZombieChild_Death : EnemyStateNodeBase<EnemyBase>
+{
+    public struct Parametor
+    {
+
+    }
+
+    private Parametor m_param = new Parametor();
+
+    public StateNode_ZombieChild_Death(EnemyBase owner, Parametor parametor)
+        :base(owner)
+    {
+        m_param = parametor;
+
+
+    }
+
+    protected override void ReserveChangeComponents()
+    {
+
+    }
+
+    public override void OnStart()
+    {
+        base.OnStart();
+    }
+
+    public override void OnUpdate()
+    {
+        Debug.Log("Death");
+    }
+
+    public override void OnExit()
+    {
+        base.OnExit();
+    }
+}
