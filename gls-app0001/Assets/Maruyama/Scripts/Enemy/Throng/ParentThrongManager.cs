9c779229aaa31b21bdf8caa59400e0ecf2b007e7
Maru <zeal20300058@fsgcl.jp>
2022-01-31 02:00:47 +0900

    2021WIZ-726
    ParentThrongManagerの生成

---
+++ gls-app0001/Assets/Maruyama/Scripts/Enemy/Throng/ParentThrongManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//親集団行動管理
+public class ParentThrongManager : MonoBehaviour
+{
+    [System.Serializable]
+    public struct Parametor
+    {
+        public float range;
+        [SerializeField]
+        private int sides;
+        public float Sides => (float)sides;
+    }
+
+    [SerializeField]
+    private Parametor m_param = new Parametor();
+    private List<ThrongData> m_throngDatas = new List<ThrongData>();
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if(m_throngDatas.Count != 0)
+        {
+            ThrongUpdate();
+        }
+    }
+
+    private void ThrongUpdate()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
