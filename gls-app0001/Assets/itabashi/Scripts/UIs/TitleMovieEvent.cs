272a38c226b548fbde5a96b09cdbcc72fbd445e3
Itabashi Oota <itaoo0430@gmail.com>
2022-01-30 14:40:06 +0900

    2021WIZ-727
    タイトルムービーイベント用コンポーネントを作成

---
+++ gls-app0001/Assets/itabashi/Scripts/UIs/TitleMovieEvent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TitleMovieEvent : MonoBehaviour
+{
+    [SerializeField]
+    private EventVideoPlayer m_eventVideoPlayer;
+
+    [SerializeField]
+    private UnityEvent m_finishEvent;
+
+    private bool m_isFinish = false;
+
+    private UIControls m_uiControls;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_uiControls = new UIControls();
+
+        this.RegisterController(m_uiControls);
+
+        m_uiControls.UI.Submit.performed += context => FinishEvent(false);
+    }
+
+    public void FinishEvent(bool forcedStop)
+    {
+        if (!forcedStop && (!m_eventVideoPlayer.IsPlaying || m_isFinish))
+        {
+            return;
+        }
+
+        m_isFinish = true;
+
+        m_finishEvent.Invoke();
+
+    }
+}
